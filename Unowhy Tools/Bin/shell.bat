a57e5b943006a275c065d18ed4b6596c845c3612
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-04-22 10:52:05 +0200

    Removed "bin"

--- Unowhy Tools/Bin/shell.bat
+++ Unowhy Tools/Bin/shell.bat
@@ -1,6 +1,6 @@
 @echo off
 echo ===Registering the REG file...===
-cd "C:\Program Files (x86)\Unowhy-Tools\"
+cd "C:\Program Files (x86)\Unowhy-Tools"
 reg import ".\shell.reg"
 echo ===Done===
 exit
