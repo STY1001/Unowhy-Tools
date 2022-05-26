9d562f366e0034a01dd32cf76a411b7e529dbcc9
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-05-26 15:00:11 +0200

    Reboot prompt

---
+++ Unowhy Tools/reboot.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Unowhy_Tools
+{
+    partial class reboot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(reboot));
+            this.msg = new System.Windows.Forms.Label();
+            this.ok = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.restart = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // msg
+            // 
+            this.msg.AutoSize = true;
+            this.msg.ForeColor = System.Drawing.Color.White;
+            this.msg.Location = new System.Drawing.Point(53, 21);
+            this.msg.Name = "msg";
+            this.msg.Size = new System.Drawing.Size(98, 13);
+            this.msg.TabIndex = 0;
+            this.msg.Text = "A reboot is required";
+            // 
+            // ok
+            // 
+            this.ok.Location = new System.Drawing.Point(181, 39);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(32, 23);
+            this.ok.TabIndex = 1;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Unowhy_Tools.Properties.Resources.reboot;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // restart
+            // 
+            this.restart.Location = new System.Drawing.Point(81, 39);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(73, 23);
+            this.restart.TabIndex = 1;
+            this.restart.Text = "Reboot";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Unowhy_Tools.Properties.Resources.reboot;
+            this.pictureBox2.Location = new System.Drawing.Point(60, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(18, 18);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Unowhy_Tools.Properties.Resources.yes;
+            this.pictureBox3.Location = new System.Drawing.Point(160, 41);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(18, 18);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // reboot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(217, 65);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.msg);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "reboot";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reboot";
+            this.Load += new System.EventHandler(this.reboot_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label msg;
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
---
+++ Unowhy Tools/reboot.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Resources;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Unowhy_Tools
+{
+    public partial class reboot : Form
+    {
+
+        public string resxFile = "null";
+
+        //Set dark mode title bar
+        [DllImport("DwmApi")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            if (DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4) != 0)
+                DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
+        }
+
+
+        public reboot()
+        {
+            RegistryKey utl = Registry.CurrentUser.OpenSubKey(@"Software\STY1001\Unowhy Tools", false);
+            string utls = utl.GetValue("Lang").ToString();
+
+            string enresx = @".\en.resx";
+            string frresx = @".\fr.resx";
+            //Chose the ResX file
+            if (utls == "EN") resxFile = enresx;    //English   
+            else resxFile = frresx;               //French
+
+            InitializeComponent();
+
+            ResXResourceSet resxSet = new ResXResourceSet(resxFile);
+
+
+            //Apply Language
+            msg.Text = resxSet.GetString("reboot");
+            restart.Text = resxSet.GetString("restart");
+
+        }
+
+        private void reboot_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(".\\reboot.exe");
+        }
+    }
+}
