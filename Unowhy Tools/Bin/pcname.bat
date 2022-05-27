b45c47f1f6d81cf2ea2248669b25d8781aa2b10a
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-05-27 12:28:18 +0200

    Write the Windows PC Name in file

---
+++ Unowhy Tools/Bin/pcname.bat
@@ -0,0 +1,14 @@
+@echo off
+title Unowhy Tools by STY1001
+echo =============================
+echo    Unowhy Tools by STY1001   
+echo
+echo        Please wait...
+echo      Veuillez patienter...
+echo 
+echo         Do not close
+echo         Ne pas fermer
+echo =============================
+MODE CON: COLS=30 LINES=10
+powershell -windows minimize -command ""
+hostname > pcname.txt
