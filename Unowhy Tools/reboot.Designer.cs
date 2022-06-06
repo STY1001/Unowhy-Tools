e632b85a8f80cb362a5ce378ead9b3d9f2d31d6b
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-06-06 19:40:30 +0200

    Window bug fix

--- Unowhy Tools/reboot.Designer.cs
+++ Unowhy Tools/reboot.Designer.cs
@@ -114,6 +114,7 @@ namespace Unowhy_Tools
             this.Controls.Add(this.ok);
             this.Controls.Add(this.msg);
             this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
