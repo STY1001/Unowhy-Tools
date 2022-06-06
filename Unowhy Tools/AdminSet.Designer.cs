9c84485d0df21de497f7d12b747efb32fad4d470
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-06-06 18:47:43 +0200

    New feature

---
+++ Unowhy Tools/AdminSet.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Unowhy_Tools
+{
+    partial class AdminSet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminSet));
+            this.disable = new System.Windows.Forms.Button();
+            this.enable = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.passbox = new System.Windows.Forms.TextBox();
+            this.passbtn = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // disable
+            // 
+            this.disable.Location = new System.Drawing.Point(268, 38);
+            this.disable.Name = "disable";
+            this.disable.Size = new System.Drawing.Size(124, 23);
+            this.disable.TabIndex = 0;
+            this.disable.Text = "Disable account";
+            this.disable.UseVisualStyleBackColor = true;
+            this.disable.Click += new System.EventHandler(this.disable_Click);
+            // 
+            // enable
+            // 
+            this.enable.Location = new System.Drawing.Point(268, 9);
+            this.enable.Name = "enable";
+            this.enable.Size = new System.Drawing.Size(124, 23);
+            this.enable.TabIndex = 1;
+            this.enable.Text = "Enable account";
+            this.enable.UseVisualStyleBackColor = true;
+            this.enable.Click += new System.EventHandler(this.enable_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Unowhy_Tools.Properties.Resources.enable;
+            this.pictureBox1.Location = new System.Drawing.Point(241, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Unowhy_Tools.Properties.Resources.disable;
+            this.pictureBox2.Location = new System.Drawing.Point(241, 37);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // passbox
+            // 
+            this.passbox.BackColor = System.Drawing.Color.Black;
+            this.passbox.ForeColor = System.Drawing.Color.White;
+            this.passbox.Location = new System.Drawing.Point(12, 12);
+            this.passbox.Name = "passbox";
+            this.passbox.PasswordChar = '•';
+            this.passbox.Size = new System.Drawing.Size(201, 20);
+            this.passbox.TabIndex = 3;
+            // 
+            // passbtn
+            // 
+            this.passbtn.Location = new System.Drawing.Point(42, 38);
+            this.passbtn.Name = "passbtn";
+            this.passbtn.Size = new System.Drawing.Size(171, 23);
+            this.passbtn.TabIndex = 4;
+            this.passbtn.Text = "Set new password";
+            this.passbtn.UseVisualStyleBackColor = true;
+            this.passbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Unowhy_Tools.Properties.Resources.key;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 34);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // AdminSet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(403, 70);
+            this.Controls.Add(this.passbtn);
+            this.Controls.Add(this.passbox);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.enable);
+            this.Controls.Add(this.disable);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AdminSet";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AdminSettings";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button disable;
+        private System.Windows.Forms.Button enable;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox passbox;
+        private System.Windows.Forms.Button passbtn;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
---
+++ Unowhy Tools/AdminSet.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unowhy_Tools
+{
+    public partial class AdminSet : Form
+    {
+        public string resxFile = "null";
+
+        //Set dark mode title bar
+
+        [DllImport("DwmApi")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            if (DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4) != 0)
+                DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
+        }
+
+        public AdminSet()
+        {
+
+            //Check the current saved language
+
+            RegistryKey utl = Registry.CurrentUser.OpenSubKey(@"Software\STY1001\Unowhy Tools", false);
+            string utls = utl.GetValue("Lang").ToString();
+
+            string enresx = @".\en.resx";
+            string frresx = @".\fr.resx";
+            //Chose the ResX file
+            if (utls == "EN") resxFile = enresx;    //English   
+            else resxFile = frresx;               //French
+
+            InitializeComponent();
+
+            ResXResourceSet resxSet = new ResXResourceSet(resxFile);
+
+            this.Text = resxSet.GetString("adminset");
+            enable.Text = resxSet.GetString("enablea");
+            disable.Text = resxSet.GetString("disablea");
+            passbtn.Text = resxSet.GetString("snpw");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string msg = passbtn.Text;
+            dialog d = new dialog(msg);
+            d.ShowDialog();
+            if (d.DialogResult.Equals(DialogResult.Yes))
+            {
+                string pass = passbox.Text;
+                string arg = ($"user Administrateur \"{pass}\"");
+                Process p = new Process();
+                p.StartInfo.FileName = "net";
+                p.StartInfo.Arguments = arg;
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                p.Start();
+                p.WaitForExit();
+            }
+        }
+
+        private void enable_Click(object sender, EventArgs e)
+        {
+            string msg = enable.Text;
+            dialog d = new dialog(msg);
+            d.ShowDialog();
+            if (d.DialogResult.Equals(DialogResult.Yes))
+            {
+                Process p = new Process();
+                p.StartInfo.FileName = ".\\enadmin.exe";
+                p.StartInfo.Arguments = "";
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                p.Start();
+                p.WaitForExit();
+            }
+        }
+
+        private void disable_Click(object sender, EventArgs e)
+        {
+            string msg = disable.Text;
+            dialog d = new dialog(msg);
+            d.ShowDialog();
+            if (d.DialogResult.Equals(DialogResult.Yes))
+            {
+                Process p = new Process();
+                p.StartInfo.FileName = ".\\disadmin.exe";
+                p.StartInfo.Arguments = "";
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                p.Start();
+                p.WaitForExit();
+            }
+        }
+    }
+}
