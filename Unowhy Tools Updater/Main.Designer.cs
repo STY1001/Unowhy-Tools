7a8a8b8fb0c5d0175d11434530eeb02a394c9db3
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-06-08 15:56:33 +0200

    New Updater

---
+++ Unowhy Tools Updater/Main.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Unowhy_Tools_Updater
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lab = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Unowhy_Tools_Updater.Properties.Resources.update;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.ForeColor = System.Drawing.Color.White;
+            this.lab.Location = new System.Drawing.Point(67, 17);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(133, 13);
+            this.lab.TabIndex = 1;
+            this.lab.Text = "Updating Unowhy Tools ...";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.ForeColor = System.Drawing.Color.White;
+            this.status.Location = new System.Drawing.Point(67, 45);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(37, 13);
+            this.status.TabIndex = 2;
+            this.status.Text = "Status";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(346, 75);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.lab);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Unowhy Tools Updater";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lab;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ Unowhy Tools Updater/Main.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Resources;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Text;
+
+namespace Unowhy_Tools_Updater
+{
+    public partial class Main : Form
+    {
+        public string resxFile = "null";
+
+        //Set dark mode title bar
+
+        [DllImport("DwmApi")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            if (DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4) != 0)
+                DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
+        }
+
+        public Main()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "taskkill";
+            p.StartInfo.Arguments = "/f /im \"Unowhy Tools.exe\"";
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            p.Start();
+            p.WaitForExit();
+
+            //Check the current saved language
+
+            RegistryKey utl = Registry.CurrentUser.OpenSubKey(@"Software\STY1001\Unowhy Tools", false);
+            string utls = utl.GetValue("Lang").ToString();
+
+            string enresx = @".\en.resx";
+            string frresx = @".\fr.resx";
+            //Chose the ResX file
+            if (utls == "EN") resxFile = enresx;    //English   
+            else resxFile = frresx;               //French
+
+            InitializeComponent();
+
+            ResXResourceSet resxSet = new ResXResourceSet(resxFile);
+
+            lab.Text = resxSet.GetString("update.updating");
+            status.Text = resxSet.GetString("update.dl");
+
+            WebClient client = new WebClient();
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(dl_complete);
+            client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Update/update.zip"), ".\\update.zip");
+
+        }
+
+        private void dl_complete(object sender, AsyncCompletedEventArgs e)
+        {
+            //Check the current saved language
+
+            RegistryKey utl = Registry.CurrentUser.OpenSubKey(@"Software\STY1001\Unowhy Tools", false);
+            string utls = utl.GetValue("Lang").ToString();
+
+            string enresx = @".\en.resx";
+            string frresx = @".\fr.resx";
+                                                     //Chose the ResX file
+            if (utls == "EN") resxFile = enresx;    //English   
+            else resxFile = frresx;                //French
+            ResXResourceSet resxSet = new ResXResourceSet(resxFile);
+
+            status.Text = resxSet.GetString("update.ext");
+
+            Process p = new Process();
+            p.StartInfo.FileName = ".\\7zip.exe";
+            p.StartInfo.Arguments = "e \"update.zip\" -aoa";
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            p.Start();
+            p.WaitForExit();
+
+            System.Diagnostics.Process.Start(".\\Unowhy Tools.exe");
+
+            this.Close();
+        }
+    }
+}
