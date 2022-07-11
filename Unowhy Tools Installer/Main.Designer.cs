49cc8281351c1ae90103f22ffe402c75497b73f4
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-07-11 10:47:53 +0200

    The new functional installer

---
+++ Unowhy Tools Installer/Main.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Unowhy_Tools_Installer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.desktop = new System.Windows.Forms.CheckBox();
+            this.run = new System.Windows.Forms.CheckBox();
+            this.install = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.statusbar = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sty1001 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.status = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // desktop
+            // 
+            this.desktop.AutoSize = true;
+            this.desktop.Checked = true;
+            this.desktop.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.desktop.ForeColor = System.Drawing.Color.White;
+            this.desktop.Location = new System.Drawing.Point(43, 155);
+            this.desktop.Name = "desktop";
+            this.desktop.Size = new System.Drawing.Size(252, 17);
+            this.desktop.TabIndex = 1;
+            this.desktop.Text = "Shortcut on desktop / Raccourcie sur le bureau";
+            this.desktop.UseVisualStyleBackColor = true;
+            // 
+            // run
+            // 
+            this.run.AutoSize = true;
+            this.run.Checked = true;
+            this.run.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.run.ForeColor = System.Drawing.Color.White;
+            this.run.Location = new System.Drawing.Point(43, 181);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(236, 17);
+            this.run.TabIndex = 1;
+            this.run.Text = "Run after installed / Lancer après installation";
+            this.run.UseVisualStyleBackColor = true;
+            // 
+            // install
+            // 
+            this.install.Location = new System.Drawing.Point(196, 254);
+            this.install.Name = "install";
+            this.install.Size = new System.Drawing.Size(100, 23);
+            this.install.TabIndex = 3;
+            this.install.Text = "Install / Installer";
+            this.install.UseVisualStyleBackColor = true;
+            this.install.Click += new System.EventHandler(this.install_Click);
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(42, 254);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(100, 23);
+            this.cancel.TabIndex = 3;
+            this.cancel.Text = "Cancel / Annuler";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // statusbar
+            // 
+            this.statusbar.Location = new System.Drawing.Point(15, 207);
+            this.statusbar.Name = "statusbar";
+            this.statusbar.Size = new System.Drawing.Size(279, 15);
+            this.statusbar.Step = 1;
+            this.statusbar.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(51, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(208, 16);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "A tool for Unowhy Y13 computers !";
+            // 
+            // sty1001
+            // 
+            this.sty1001.AutoSize = true;
+            this.sty1001.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.sty1001.ForeColor = System.Drawing.Color.White;
+            this.sty1001.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.sty1001.Location = new System.Drawing.Point(63, 126);
+            this.sty1001.Name = "sty1001";
+            this.sty1001.Size = new System.Drawing.Size(185, 13);
+            this.sty1001.TabIndex = 35;
+            this.sty1001.Text = "by STY1001 | sty1001.wordpress.com\r\n";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Unowhy_Tools_Installer.Properties.Resources.no;
+            this.pictureBox5.Location = new System.Drawing.Point(14, 253);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 2;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Unowhy_Tools_Installer.Properties.Resources.download;
+            this.pictureBox4.Location = new System.Drawing.Point(168, 253);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Unowhy_Tools_Installer.Properties.Resources.startup;
+            this.pictureBox3.Location = new System.Drawing.Point(16, 178);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Unowhy_Tools_Installer.Properties.Resources.desktop;
+            this.pictureBox2.Location = new System.Drawing.Point(16, 152);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Unowhy_Tools_Installer.Properties.Resources.UTLogoWhite2_0;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 101);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // status
+            // 
+            this.status.BackColor = System.Drawing.Color.Black;
+            this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.ForeColor = System.Drawing.Color.White;
+            this.status.Location = new System.Drawing.Point(15, 227);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(279, 20);
+            this.status.TabIndex = 37;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(310, 292);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sty1001);
+            this.Controls.Add(this.statusbar);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.install);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.desktop);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Unowhy Tools Installer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox desktop;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox run;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button install;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.ProgressBar statusbar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label sty1001;
+        private System.Windows.Forms.TextBox status;
+    }
+}
+
---
+++ Unowhy Tools Installer/Main.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Net;
+using System.Diagnostics;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Taskbar;
+
+namespace Unowhy_Tools_Installer
+{
+    public partial class Main : Form
+    {
+        [DllImport("DwmApi")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 35, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 38, new[] { 1 }, 4);
+        }
+
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int state, int value);
+
+        public Main()
+        {
+            InitializeComponent();
+
+            status.Text = "";
+        }
+
+        private static void delay(int Time_delay)
+        {
+            int i = 0;
+            System.Timers.Timer _delayTimer = new System.Timers.Timer();
+            _delayTimer.Interval = Time_delay;
+            _delayTimer.AutoReset = false; 
+            _delayTimer.Elapsed += (s, args) => i = 1;
+            _delayTimer.Start();
+            while (i == 0) { };
+        }
+
+        private void install_pre()
+        {
+            statusbar.Value = 0;
+            TaskbarManager.Instance.SetProgressValue(0, 100);
+            status.Text = "Preparing...";
+            delay(1000);
+            delay(600);
+
+            if (Directory.Exists("C:\\Program Files (x86)\\Unowhy Tools"))
+            {
+                Directory.Delete("C:\\Program Files (x86)\\Unowhy Tools", true);
+                delay(300);
+                Directory.CreateDirectory("C:\\Program Files (x86)\\Unowhy Tools");
+            }
+            else
+            {
+                Directory.CreateDirectory("C:\\Program Files (x86)\\Unowhy Tools");
+            }
+
+            delay(300);
+            statusbar.Value = 5;
+            TaskbarManager.Instance.SetProgressValue(5, 100);
+            delay(600);
+
+            if (Directory.Exists("C:\\Program Files (x86)\\Unowhy Tools\\insttemp") == true)
+            {
+                Directory.Delete("C:\\Program Files (x86)\\Unowhy Tools\\insttemp", true);
+                delay(300);
+                Directory.CreateDirectory("C:\\Program Files (x86)\\Unowhy Tools\\insttemp");
+            }
+            else
+            {
+                Directory.CreateDirectory("C:\\Program Files (x86)\\Unowhy Tools\\insttemp");
+            }
+
+            delay(300);
+            statusbar.Value = 10;
+            TaskbarManager.Instance.SetProgressValue(10, 100);
+
+            install_dl();
+        }
+
+        private void install_dl()
+        {
+            status.Text = "Downloading...";
+            delay(1000);
+
+            using (var client = new WebClient())
+            {
+                status.Text = "Downloading... (\"7zip.exe\")";
+                delay(600);
+                client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Extras/7zip.exe"), "C:\\Program Files (x86)\\Unowhy Tools\\7zip.exe");
+                statusbar.Value = 15;
+                TaskbarManager.Instance.SetProgressValue(15, 100);
+                status.Text = "Downloading... (\"7z.dll\")";
+                delay(1000);
+                client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Extras/7z.dll"), "C:\\Program Files (x86)\\Unowhy Tools\\7z.dll");
+                statusbar.Value = 20;
+                TaskbarManager.Instance.SetProgressValue(20, 100);
+                status.Text = "Downloading... (\"utkeyinst.reg\")";
+                delay(600);
+                client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Install/utkeyinst.reg"), "C:\\Program Files (x86)\\Unowhy Tools\\insttemp\\utkeyinst.reg");
+                statusbar.Value = 25;
+                TaskbarManager.Instance.SetProgressValue(25, 100);
+                status.Text = "Downloading... (\"utkeyinst.exe\")";
+                delay(600);
+                client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Install/utkeyinst.exe"), "C:\\Program Files (x86)\\Unowhy Tools\\insttemp\\utkeyinst.exe");
+                statusbar.Value = 30;
+                TaskbarManager.Instance.SetProgressValue(30, 100);
+                status.Text = "Downloading... (\"uninstall.exe\")";
+                delay(600);
+                client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Uninstall/uninstall.exe"), "C:\\Program Files (x86)\\Unowhy Tools\\uninstall.exe");
+                statusbar.Value = 35;
+                TaskbarManager.Instance.SetProgressValue(35, 100);
+                status.Text = "Downloading... (\"utconfdel.exe\")";
+                delay(300);
+                client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Uninstall/utconfdel.exe"), "C:\\Program Files (x86)\\Unowhy Tools\\utconfdel.exe");
+                statusbar.Value = 40;
+                TaskbarManager.Instance.SetProgressValue(40, 100);
+                status.Text = "Downloading... (\"utkeydel.exe\")";
+                delay(600);
+                client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Uninstall/utkeydel.exe"), "C:\\Program Files (x86)\\Unowhy Tools\\utkeydel.exe");
+                statusbar.Value = 45;
+                TaskbarManager.Instance.SetProgressValue(45, 100);
+                status.Text = "Downloading... (\"fr.resx\")";
+                delay(600);
+                client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Unowhy%20Tools/Lang/fr.resx"), "C:\\Program Files (x86)\\Unowhy Tools\\fr.resx");
+                statusbar.Value = 50;
+                TaskbarManager.Instance.SetProgressValue(50, 100);
+                status.Text = "Downloading... (\"en.resx\")";
+                delay(600);
+                client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Unowhy%20Tools/Lang/en.resx"), "C:\\Program Files (x86)\\Unowhy Tools\\en.resx");
+                statusbar.Value = 55;
+                TaskbarManager.Instance.SetProgressValue(55, 100);
+                status.Text = "Downloading... (\"update.exe\")";
+                delay(1000);
+                client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Update/update.zip"), "C:\\Program Files (x86)\\Unowhy Tools\\update.zip");
+                statusbar.Value = 60;
+                TaskbarManager.Instance.SetProgressValue(60, 100);
+                status.Text = "Installing...";
+                delay(600);
+
+                install_inst();
+            }
+        }
+
+        private void install_inst()
+        {
+            
+            delay(1000);
+
+            Process p1 = new Process();
+            p1.StartInfo.FileName = "C:\\Program Files (x86)\\Unowhy Tools\\7zip.exe";
+            p1.StartInfo.Arguments = "e \"C:\\Program Files (x86)\\Unowhy Tools\\update.zip\" -aoa";
+            p1.StartInfo.WorkingDirectory = "C:\\Program Files (x86)\\Unowhy Tools";
+            p1.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            p1.Start();
+            p1.WaitForExit();
+
+            statusbar.Value = 65;
+            TaskbarManager.Instance.SetProgressValue(65, 100);
+            delay(1000);
+
+            Process p2 = new Process();
+            p2.StartInfo.FileName = "C:\\Program Files (x86)\\Unowhy Tools\\insttemp\\utkeyinst.exe";
+            p2.StartInfo.Arguments = "";
+            p2.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            p2.Start();
+            p2.WaitForExit();
+
+            statusbar.Value = 70;
+            TaskbarManager.Instance.SetProgressValue(70, 100);
+            install_post();
+        }
+
+        private void install_post()
+        {
+            delay(1000);
+            status.Text = "Finalizing...";
+            delay(1000);
+
+            using (var client = new WebClient())
+            {
+                client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Install/Unowhy%20Tools.lnk"), "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Unowhy Tools.lnk");
+            }
+
+            statusbar.Value = 75;
+            TaskbarManager.Instance.SetProgressValue(75, 100);
+            delay(1000);
+
+            if (desktop.Checked == true)
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Install/Unowhy%20Tools.lnk"), "C:\\Users\\Public\\Desktop\\Unowhy Tools.lnk");
+                }
+            }
+
+            statusbar.Value = 80;
+            TaskbarManager.Instance.SetProgressValue(80, 100);
+            delay(1000);
+            install_clean();
+        }
+        
+        private void install_clean()
+        {
+            status.Text = "Cleaning...";
+            delay(1000);
+
+            if (Directory.Exists("C:\\Program Files (x86)\\Unowhy Tools\\insttemp"))
+            {
+                Directory.Delete("C:\\Program Files (x86)\\Unowhy Tools\\insttemp", true);
+            }
+
+            statusbar.Value = 85;
+            TaskbarManager.Instance.SetProgressValue(85, 100);
+            delay(1000);
+
+            if (Directory.Exists("C:\\Program Files (x86)\\Unowhy Tools\\update"))
+            {
+                Directory.Delete("C:\\Program Files (x86)\\Unowhy Tools\\update", true);
+            }
+
+            statusbar.Value = 90;
+            TaskbarManager.Instance.SetProgressValue(90, 100);
+            delay(1000);
+
+            if (File.Exists("C:\\Program Files (x86)\\Unowhy Tools\\update.zip"))
+            {
+                File.Delete("C:\\Program Files (x86)\\Unowhy Tools\\update.zip");
+            }
+
+            statusbar.Value = 95;
+            TaskbarManager.Instance.SetProgressValue(95, 100);
+            delay(1000);
+
+            if (Directory.Exists(".\\temp"))
+            {
+                Directory.Delete(".\\temp", true);
+            }
+
+            delay(1000);
+            install_finish();
+        }
+
+        private void install_finish()
+        {
+            statusbar.Value = 100;
+            TaskbarManager.Instance.SetProgressValue(100, 100);
+            status.Text = "Finished !";
+            delay(3000);
+            if (run.Checked == true)
+            {
+                System.Diagnostics.Process.Start("C:\\Program Files (x86)\\Unowhy Tools\\Unowhy Tools.exe");
+            }
+
+            delay(100);
+
+            this.Close();
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void install_Click(object sender, EventArgs e)
+        {
+            string noco = "0";
+
+            int Out;
+            if (InternetGetConnectedState(out Out, 0) == true) noco = "0";
+            else noco = "1";
+
+            if(noco == "1")
+            {
+                var co = new noco();
+                TaskbarManager.Instance.SetProgressValue(100, 100);
+                TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Error);
+                co.ShowDialog();
+                TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.NoProgress);
+            }
+            else
+            {
+                desktop.Enabled = false;
+                run.Enabled = false;
+                cancel.Enabled = false;
+                install.Enabled = false;
+
+                install_pre();
+            }
+        }
+    }
+}
