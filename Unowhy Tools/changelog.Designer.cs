598dc28a9a2dce9830bc08f352eda82cdfc7b26e
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-08-29 20:49:58 +0200

    Change Log work fine

--- Unowhy Tools/changelog.Designer.cs
+++ Unowhy Tools/changelog.Designer.cs
@@ -29,27 +29,68 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(changelog));
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webclog = new System.Windows.Forms.WebBrowser();
+            this.webdown = new ReaLTaiizor.Controls.Button();
+            this.webup = new ReaLTaiizor.Controls.Button();
             this.SuspendLayout();
             // 
-            // webBrowser1
+            // webclog
             // 
-            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.ScriptErrorsSuppressed = true;
-            this.webBrowser1.Size = new System.Drawing.Size(800, 450);
-            this.webBrowser1.TabIndex = 0;
-            this.webBrowser1.Url = new System.Uri("https://bit.ly/UTCLogIMG", System.UriKind.Absolute);
+            this.webclog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webclog.Location = new System.Drawing.Point(0, 0);
+            this.webclog.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webclog.Name = "webclog";
+            this.webclog.ScriptErrorsSuppressed = true;
+            this.webclog.ScrollBarsEnabled = false;
+            this.webclog.Size = new System.Drawing.Size(813, 450);
+            this.webclog.TabIndex = 0;
+            this.webclog.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // webdown
+            // 
+            this.webdown.BackColor = System.Drawing.Color.Transparent;
+            this.webdown.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.webdown.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.webdown.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.webdown.Image = null;
+            this.webdown.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.webdown.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.webdown.Location = new System.Drawing.Point(781, 420);
+            this.webdown.Name = "webdown";
+            this.webdown.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.webdown.Size = new System.Drawing.Size(32, 30);
+            this.webdown.TabIndex = 1;
+            this.webdown.Text = "▼";
+            this.webdown.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.webdown.Click += new System.EventHandler(this.webdown_Click);
+            // 
+            // webup
+            // 
+            this.webup.BackColor = System.Drawing.Color.Transparent;
+            this.webup.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.webup.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.webup.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.webup.Image = null;
+            this.webup.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.webup.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.webup.Location = new System.Drawing.Point(781, 0);
+            this.webup.Name = "webup";
+            this.webup.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.webup.Size = new System.Drawing.Size(32, 30);
+            this.webup.TabIndex = 1;
+            this.webup.Text = "▲";
+            this.webup.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.webup.Click += new System.EventHandler(this.webup_Click);
             // 
             // changelog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.webBrowser1);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(813, 450);
+            this.Controls.Add(this.webup);
+            this.Controls.Add(this.webdown);
+            this.Controls.Add(this.webclog);
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -64,6 +105,8 @@
 
         #endregion
 
-        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.WebBrowser webclog;
+        private ReaLTaiizor.Controls.Button webdown;
+        private ReaLTaiizor.Controls.Button webup;
     }
 }
--- Unowhy Tools/changelog.cs
+++ Unowhy Tools/changelog.cs
@@ -22,6 +22,8 @@ namespace Unowhy_Tools
         [DllImport("DwmApi")]
         private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
 
+        public int scrollTop = 0;
+
         protected override void OnHandleCreated(EventArgs e)
         {
             DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4);
@@ -33,6 +35,101 @@ namespace Unowhy_Tools
         public changelog()
         {
             InitializeComponent();
+            string path = Directory.GetCurrentDirectory();
+            string urlp = "file://" + path + "\\clog.html";
+            webclog.Navigate(urlp);
+        }
+
+        private void webup_Click(object sender, EventArgs e)
+        {
+            up();
+        }
+
+        private void webdown_Click(object sender, EventArgs e)
+        {
+            down();
+        }
+
+        private void up()
+        {
+            HtmlDocument htmlDoc = webclog.Document;
+            int scrollLeft = htmlDoc.GetElementsByTagName("HTML")[0].ScrollLeft;
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop > 180 ? scrollTop - 10 : 0;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+        }
+
+        private void down()
+        {
+            HtmlDocument htmlDoc = webclog.Document;
+            int scrollLeft = htmlDoc.GetElementsByTagName("HTML")[0].ScrollLeft;
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
+            scrollTop = scrollTop + 10;
+            webclog.Document.Window.ScrollTo(new Point(scrollLeft, scrollTop));
         }
     }
 }
