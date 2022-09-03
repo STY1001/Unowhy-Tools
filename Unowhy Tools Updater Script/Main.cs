23a319f5edd36e2abafcd615d35c809efa7d8779
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-09-03 15:28:13 +0200

    Bug Fix

--- Unowhy Tools Updater Script/Main.cs
+++ Unowhy Tools Updater Script/Main.cs
@@ -46,55 +46,6 @@ namespace Unowhy_Tools_Updater_Script
         {
             InitializeComponent();
 
-            //TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Indeterminate);
-
-            string dest = Path.GetTempPath() + "\\Unowhy Tools Installer.exe";
-
-            if (File.Exists("old2new"))
-            {
-
-            }
-            else
-            {
-                using (var client = new WebClient())
-                {
-                    client.DownloadFile("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Update/script/utdeloldkey.exe", ".\\utdeloldkey.exe");
-                }
-
-                delay(1000);
-
-                Process p1 = new Process();
-                p1.StartInfo.FileName = ".\\utdeloldkey.exe";
-                p1.StartInfo.Arguments = "";
-                p1.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
-                p1.Start();
-                p1.WaitForExit();
-
-                delay(1000);
-
-                File.Delete("utdeloldkey.exe");
-
-                if (Directory.Exists("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Unowhy Tools"))
-                {
-                    Directory.Delete("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Unowhy Tools", true);
-                }
-
-                if (File.Exists("C:\\Users\\Public\\Desktop\\Unowhy Tools.lnk"))
-                {
-                    File.Delete("C:\\Users\\Public\\Desktop\\Unowhy Tools.lnk");
-                }
-
-                using (var client = new WebClient())
-                {
-                    client.DownloadFile("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Update/script/Unowhy%20Tools%20Installer.exe", dest);
-                }
-
-                delay(1000);
-
-                System.Diagnostics.Process.Start(dest);
-
-            }
-
             this.Close();
         }
     }
