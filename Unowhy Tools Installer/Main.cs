92bf4f0bf7c460737629df8e5c7237a9fb60254b
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-10-20 21:09:38 +0200

    Extract func for offline install

--- Unowhy Tools Installer/Main.cs
+++ Unowhy Tools Installer/Main.cs
@@ -14,6 +14,7 @@ using System.Net;
 using System.Diagnostics;
 using Microsoft.WindowsAPICodePack.Shell;
 using Microsoft.WindowsAPICodePack.Taskbar;
+using System.Reflection;
 
 namespace Unowhy_Tools_Installer
 {
@@ -42,6 +43,23 @@ namespace Unowhy_Tools_Installer
             System.Diagnostics.Process.Start("taskkill", "/f /im \"uninstall.exe\"");
 
             status.Text = "";
+        }
+
+        public static void Extract(string nameSpace, string outDirectory, string internalFilePath, string resourceName)
+        {
+            //nameSpace = Project
+            //outDirectory = Out Folder
+            //internalFilePath = Res Folder
+            //resourceName = Res Name
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            using (Stream s = assembly.GetManifestResourceStream(nameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName))
+            using (BinaryReader r = new BinaryReader(s))
+            using (FileStream fs = new FileStream(outDirectory + "\\" + resourceName, FileMode.OpenOrCreate))
+            using (BinaryWriter w = new BinaryWriter(fs))
+            {
+                w.Write(r.ReadBytes((int)s.Length));
+            }
         }
 
         private static void delay(int Time_delay)
@@ -101,7 +119,7 @@ namespace Unowhy_Tools_Installer
         {
             status.Text = "Downloading...";
             delay(1000);
-            using (var client = new WebClient())
+            /*using (var client = new WebClient())
             {
                 client.DownloadFile("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Extras/7zip.exe", "C:\\Program Files (x86)\\Unowhy Tools\\7zip.exe");
             }
@@ -169,7 +187,7 @@ namespace Unowhy_Tools_Installer
                 client.DownloadFile("https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Install/install.zip", "C:\\Program Files (x86)\\Unowhy Tools\\update.zip");
             }
             statusbar.Value = 60;
-            TaskbarManager.Instance.SetProgressValue(60, 100);
+            TaskbarManager.Instance.SetProgressValue(60, 100);*/
             status.Text = "Installing...";
             delay(600);
             
