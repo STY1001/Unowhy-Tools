8d648eaf2aaa056b12a1dfb6140870198374643e
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-11-01 14:47:19 +0100

    Minor UI fix

--- Unowhy Tools/Splash.Designer.cs
+++ Unowhy Tools/Splash.Designer.cs
@@ -79,7 +79,7 @@ namespace Unowhy_Tools
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 127);
+            this.label2.Location = new System.Drawing.Point(8, 119);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(191, 13);
             this.label2.TabIndex = 3;
@@ -89,7 +89,7 @@ namespace Unowhy_Tools
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(11, 109);
+            this.label3.Location = new System.Drawing.Point(7, 101);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(213, 17);
             this.label3.TabIndex = 4;
