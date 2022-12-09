c02b0fbd197edd48bdc0edcdca35a0a4c2243ea3
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-12-09 22:09:27 +0100

    New functions and new namespace

--- Unowhy Tools/UTclass.cs
+++ Unowhy Tools/UTclass.cs
@@ -1,14 +1,20 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Taskbar;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Resources;
 using System.Runtime.InteropServices;
+using System.ServiceModel;
+using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Unowhy_Tools.UTclass
+namespace Unowhy_Tools
 {
     public static class UTclass
     {
@@ -18,6 +24,29 @@ namespace Unowhy_Tools.UTclass
         [DllImport("kernel32.dll", SetLastError = true)]
         static extern bool Wow64RevertWow64FsRedirection(IntPtr ptr);
 
+        public static class serv
+        {
+            public static void stop(string service)
+            {
+                runmin("net", "stop \"" + service + "\" /y", true);
+            }
+
+            public static void start(string service)
+            {
+                runmin("net", "start \"" + service + "\"", true);
+            }
+
+            public static void auto(string service)
+            {
+                runmin("powershell", "-Name \"" + service + "\" -StartupType Automatic", true);
+            }
+
+            public static void dis(string service)
+            {
+                serv.stop(service);
+                runmin("powershell", "-Name \"" + service + "\" -StartupType Disabled", true);
+            }
+        }
 
         public static void delay(int Time_delay)
         {
@@ -64,5 +93,55 @@ namespace Unowhy_Tools.UTclass
             var lines = text.Split('\n');
             return lines[line2];
         }
+
+        public static string getlang(string name)
+        {
+            //Check the current saved language
+            string resxFile;
+            RegistryKey utl = Registry.CurrentUser.OpenSubKey(@"Software\STY1001\Unowhy Tools", false);
+            string utls = utl.GetValue("Lang").ToString();
+            string enresx = @".\en.resx";
+            string frresx = @".\fr.resx";
+            //Chose the ResX file
+            if (utls == "EN") resxFile = enresx;    //English   
+            else resxFile = frresx;               //French
+            ResXResourceSet resxSet = new ResXResourceSet(resxFile);
+            return resxSet.GetString(name);
+        }
+
+        public static void runmin(string file, string args, bool msg)
+        {
+            IntPtr wow64Value = IntPtr.Zero;
+            Wow64DisableWow64FsRedirection(ref wow64Value);
+
+            if (msg == true)
+            {
+                TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Indeterminate);
+            }
+
+            Thread t = new Thread(new ThreadStart(WaitScreen));
+            if (msg == true)
+            {
+                t.Start();
+            }
+
+            Process p = new Process();
+            p.StartInfo.FileName = file;
+            p.StartInfo.Arguments = args;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.Start();
+            p.WaitForExit();
+            if (msg == true)
+            {
+                t.Abort();
+                TaskbarManager.Instance.SetProgressState(TaskbarProgressBarState.Normal);
+            }
+        }
+
+        public static void WaitScreen()
+        {
+            Application.Run(new wait());
+        }
+
     }
 }
