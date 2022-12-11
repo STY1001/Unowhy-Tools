84c4c02de992c5d41b5a88be0f6311e125ad61e5
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-12-11 14:44:55 +0100

    Optimized Code

--- Unowhy Tools/dismdriver.cs
+++ Unowhy Tools/dismdriver.cs
@@ -20,17 +20,13 @@ using System.Windows.Input;
 using System.ServiceProcess;
 using Microsoft.WindowsAPICodePack.Shell;
 using Microsoft.WindowsAPICodePack.Taskbar;
+using static Unowhy_Tools.UTclass;
 
 namespace Unowhy_Tools
 {
     public partial class dismdriver : Form
     {
         public string resxFile = "null";
-
-        public void WaitScreen()
-        {
-            Application.Run(new wait());
-        }
 
         //Set dark mode title bar and bypass wow64 redirection
 
@@ -52,34 +48,9 @@ namespace Unowhy_Tools
             IntPtr wow64Value = IntPtr.Zero;
             Wow64DisableWow64FsRedirection(ref wow64Value);
         }
-        //Wait fonc
-
-        private static void delay(int Time_delay)
-        {
-            int i = 0;
-            System.Timers.Timer _delayTimer = new System.Timers.Timer();
-            _delayTimer.Interval = Time_delay;
-            _delayTimer.AutoReset = false;
-            _delayTimer.Elapsed += (s, args) => i = 1;
-            _delayTimer.Start();
-            while (i == 0) { };
-        }
 
         public dismdriver()
         {
-
-
-            //Check the current saved language
-
-            RegistryKey utl = Registry.CurrentUser.OpenSubKey(@"Software\STY1001\Unowhy Tools", false);
-            string utls = utl.GetValue("Lang").ToString();
-
-            string enresx = @".\en.resx";
-            string frresx = @".\fr.resx";
-            //Chose the ResX file
-            if (utls == "EN") resxFile = enresx;    //English   
-            else resxFile = frresx;               //French
-
             InitializeComponent();
 
             if (File.Exists("debug"))
@@ -87,15 +58,13 @@ namespace Unowhy_Tools
                 deb.Visible = true;
             }
 
-            ResXResourceSet resxSet = new ResXResourceSet(resxFile);
-
-            oeb.Text = resxSet.GetString("browse");
-            ieb.Text = resxSet.GetString("browse");
-            bb.Text = resxSet.GetString("backup.short");
-            rb.Text = resxSet.GetString("restore.short");
-            this.Text = resxSet.GetString("dismdrv.title");
-            labb.Text = resxSet.GetString("backup");
-            labr.Text = resxSet.GetString("restore");
+            oeb.Text = getlang("browse");
+            ieb.Text = getlang("browse");
+            bb.Text = getlang("backup.short");
+            rb.Text = getlang("restore.short");
+            this.Text = getlang("dismdrv.title");
+            labb.Text = getlang("backup");
+            labr.Text = getlang("restore");
         }
 
         private void oeb_Click(object sender, EventArgs e)
@@ -129,12 +98,7 @@ namespace Unowhy_Tools
                 Thread t = new Thread(new ThreadStart(WaitScreen));               
                 t.Start();
 
-                Process p = new Process();
-                p.StartInfo.FileName = "dism.exe";
-                p.StartInfo.Arguments = arg;
-                p.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
-                p.Start();
-                p.WaitForExit();
+                runmin("dism.exe", arg, false);
 
                 File.Copy(".\\UT-Restore.exe", opb.Text + "\\UT-Restore.exe");
 
@@ -173,30 +137,13 @@ namespace Unowhy_Tools
                 {
                     File.Copy(".\\UT-Restore.exe", ipb.Text);
                 }
-                /*
-                RegistryKey utl = Registry.CurrentUser.OpenSubKey(@"Software\STY1001\Unowhy Tools", false);
-                string utls = utl.GetValue("Lang").ToString();
-
-                string enresx = @".\en.resx";
-                string frresx = @".\fr.resx";
-                //Chose the ResX file
-                if (utls == "EN") resxFile = enresx;    //English   
-                else resxFile = frresx;               //French
-
-                ResXResourceSet resxSet = new ResXResourceSet(resxFile);
-
-                string msgtxt = resxSet.GetString("psdrvmsg");
-
-                MessageBox.Show(msgtxt, "PS Drv GUI for Unowhy Tools", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-
-                System.Diagnostics.Process.Start("explorer.exe", deb.Text);
-                */
 
                 Process p = new Process();
                 p.StartInfo.FileName = ipb.Text;
                 p.StartInfo.Arguments = "";
                 p.StartInfo.WorkingDirectory = deb.Text;
-                p.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                p.StartInfo.CreateNoWindow = true;
                 p.Start();
                 p.WaitForExit();
 
