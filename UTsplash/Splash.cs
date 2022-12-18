b603bc11fa90d4753acdf7a2c4944a7815363deb
STY1001 <5788952+STY1001@users.noreply.github.com>
2022-12-18 20:15:15 +0100

    New screen to fix thread error

---
+++ UTsplash/Splash.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Resources;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Threading;
+using System.Windows.Input;
+
+namespace UTsplash
+{
+    public partial class Splash : Form
+    {
+
+        //Set dark mode title bar
+
+        [DllImport("DwmApi")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 35, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 38, new[] { 1 }, 4);
+        }
+
+
+        public Splash()
+        {
+            InitializeComponent();
+            label4.Text = "Version 19.0";
+        }
+
+        public void close()
+        {
+            this.Close();
+        }
+
+        private void Splash_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
