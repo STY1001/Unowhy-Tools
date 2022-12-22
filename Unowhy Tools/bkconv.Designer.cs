847e8f2402080d9326a97f69b9271a7446429870
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-12-22 20:03:07 +0100

    Convert form done

---
+++ Unowhy Tools/bkconv.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Unowhy_Tools
+{
+    partial class bkconv
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(bkconv));
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labold = new System.Windows.Forms.Label();
+            this.patho = new System.Windows.Forms.TextBox();
+            this.browseo = new ReaLTaiizor.Controls.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.labnew = new System.Windows.Forms.Label();
+            this.pathn = new System.Windows.Forms.TextBox();
+            this.conv = new ReaLTaiizor.Controls.Button();
+            this.browsen = new ReaLTaiizor.Controls.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Unowhy_Tools.Properties.Resources.folder;
+            this.pictureBox4.Location = new System.Drawing.Point(266, 64);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(22, 22);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Unowhy_Tools.Properties.Resources.download;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labold
+            // 
+            this.labold.AutoSize = true;
+            this.labold.ForeColor = System.Drawing.Color.White;
+            this.labold.Location = new System.Drawing.Point(35, 16);
+            this.labold.Name = "labold";
+            this.labold.Size = new System.Drawing.Size(26, 13);
+            this.labold.TabIndex = 9;
+            this.labold.Text = "Old";
+            // 
+            // patho
+            // 
+            this.patho.BackColor = System.Drawing.Color.Black;
+            this.patho.ForeColor = System.Drawing.Color.White;
+            this.patho.Location = new System.Drawing.Point(8, 36);
+            this.patho.Name = "patho";
+            this.patho.Size = new System.Drawing.Size(337, 22);
+            this.patho.TabIndex = 8;
+            // 
+            // browseo
+            // 
+            this.browseo.BackColor = System.Drawing.Color.Transparent;
+            this.browseo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.browseo.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.browseo.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browseo.Image = null;
+            this.browseo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.browseo.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.browseo.Location = new System.Drawing.Point(292, 64);
+            this.browseo.Name = "browseo";
+            this.browseo.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.browseo.Size = new System.Drawing.Size(53, 22);
+            this.browseo.TabIndex = 7;
+            this.browseo.Text = "Browse";
+            this.browseo.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.browseo.Click += new System.EventHandler(this.browseo_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Unowhy_Tools.Properties.Resources.convert;
+            this.pictureBox2.Location = new System.Drawing.Point(266, 226);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(22, 22);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Unowhy_Tools.Properties.Resources.folder;
+            this.pictureBox3.Location = new System.Drawing.Point(266, 181);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(22, 22);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Unowhy_Tools.Properties.Resources.upload;
+            this.pictureBox6.Location = new System.Drawing.Point(12, 129);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 17;
+            this.pictureBox6.TabStop = false;
+            // 
+            // labnew
+            // 
+            this.labnew.AutoSize = true;
+            this.labnew.ForeColor = System.Drawing.Color.White;
+            this.labnew.Location = new System.Drawing.Point(35, 133);
+            this.labnew.Name = "labnew";
+            this.labnew.Size = new System.Drawing.Size(30, 13);
+            this.labnew.TabIndex = 16;
+            this.labnew.Text = "New";
+            // 
+            // pathn
+            // 
+            this.pathn.BackColor = System.Drawing.Color.Black;
+            this.pathn.ForeColor = System.Drawing.Color.White;
+            this.pathn.Location = new System.Drawing.Point(8, 153);
+            this.pathn.Name = "pathn";
+            this.pathn.Size = new System.Drawing.Size(337, 22);
+            this.pathn.TabIndex = 15;
+            // 
+            // conv
+            // 
+            this.conv.BackColor = System.Drawing.Color.Transparent;
+            this.conv.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.conv.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.conv.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.conv.Image = null;
+            this.conv.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.conv.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.conv.Location = new System.Drawing.Point(292, 226);
+            this.conv.Name = "conv";
+            this.conv.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.conv.Size = new System.Drawing.Size(53, 22);
+            this.conv.TabIndex = 13;
+            this.conv.Text = "Convert";
+            this.conv.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.conv.Click += new System.EventHandler(this.conv_Click);
+            // 
+            // browsen
+            // 
+            this.browsen.BackColor = System.Drawing.Color.Transparent;
+            this.browsen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.browsen.EnteredColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.browsen.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browsen.Image = null;
+            this.browsen.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.browsen.InactiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(34)))), ((int)(((byte)(37)))));
+            this.browsen.Location = new System.Drawing.Point(292, 181);
+            this.browsen.Name = "browsen";
+            this.browsen.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(165)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.browsen.Size = new System.Drawing.Size(53, 22);
+            this.browsen.TabIndex = 14;
+            this.browsen.Text = "Browse";
+            this.browsen.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.browsen.Click += new System.EventHandler(this.browsen_Click);
+            // 
+            // bkconv
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(361, 260);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.labnew);
+            this.Controls.Add(this.pathn);
+            this.Controls.Add(this.conv);
+            this.Controls.Add(this.browsen);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labold);
+            this.Controls.Add(this.patho);
+            this.Controls.Add(this.browseo);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "bkconv";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Convert a backup";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labold;
+        private System.Windows.Forms.TextBox patho;
+        private ReaLTaiizor.Controls.Button browseo;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label labnew;
+        private System.Windows.Forms.TextBox pathn;
+        private ReaLTaiizor.Controls.Button conv;
+        private ReaLTaiizor.Controls.Button browsen;
+    }
+}
---
+++ Unowhy Tools/bkconv.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Unowhy_Tools.UTclass;
+
+namespace Unowhy_Tools
+{
+    public partial class bkconv : Form
+    {
+        //Set dark mode title bar and bypass wow64 redirection
+
+        [DllImport("DwmApi")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool Wow64DisableWow64FsRedirection(ref IntPtr ptr);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool Wow64RevertWow64FsRedirection(IntPtr ptr);
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 35, new[] { 1 }, 4);
+            DwmSetWindowAttribute(Handle, 38, new[] { 1 }, 4);
+            IntPtr wow64Value = IntPtr.Zero;
+            Wow64DisableWow64FsRedirection(ref wow64Value);
+        }
+
+        public bkconv()
+        {
+            InitializeComponent();
+        }
+
+        private void browseo_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    patho.Text = fbd.SelectedPath;
+                    if(!File.Exists(patho.Text + "\\UT-Restore.exe"))
+                    {
+                        var d = new info(getlang("conv.nout"), Properties.Resources.no);
+                        d.ShowDialog();
+                        patho.Text = "";
+                    }
+                }
+            }
+        }
+
+        private void browsen_Click(object sender, EventArgs e)
+        {
+            using(var sfd = new SaveFileDialog())
+            {
+                sfd.FileName = "UT-Drv_" + DateTime.Now.ToString("HH-mm_dd-MM-yy");
+                sfd.DefaultExt = "zip";
+                sfd.Filter = "zip (*.zip)|*.zip";
+                sfd.FilterIndex = 1;
+                sfd.Title = "Unowhy Tools";
+                DialogResult result = sfd.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    pathn.Text = sfd.FileName;
+                }
+            }
+        }
+
+        private void conv_Click(object sender, EventArgs e)
+        {
+            if(pathn.Text == "" || patho.Text == "")
+            {
+
+            }
+            else
+            {
+                runmin("7zip.exe", "a \"" + pathn.Text + "\" \"" + patho.Text + "\\*\" -tzip -mx=0", true);
+                var d = new info(getlang("done"), Properties.Resources.yes);
+                d.ShowDialog();
+            }
+        }
+    }
+}
