60240b383a31e94564f3e7e1e28b1ce444e633bf
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-01-12 17:43:33 +0100

    Recreated project with template

---
+++ Unowhy Tools WPF/Models/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Unowhy_Tools_WPF.Models
+{
+    public class AppConfig
+    {
+        public string ConfigurationsFolder { get; set; }
+
+        public string AppPropertiesFileName { get; set; }
+    }
+}
---
+++ Unowhy Tools WPF/Models/DataColor.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Media;
+
+namespace Unowhy_Tools_WPF.Models
+{
+    public struct DataColor
+    {
+        public Brush Color { get; set; }
+    }
+}
