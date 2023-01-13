a8037cd6bbb7cb92b14d18b289bade50a0f91633
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-01-13 20:53:57 +0100

    Not done

--- Unowhy Tools WPF/Views/Pages/About.xaml.cs
+++ Unowhy Tools WPF/Views/Pages/About.xaml.cs
@@ -5,6 +5,13 @@
 
 using Wpf.Ui.Common.Interfaces;
 using Unowhy_Tools_WPF.ViewModels;
+using System.Threading.Tasks;
+using Unowhy_Tools_WPF.Views;
+using System.Windows;
+using System.Diagnostics;
+using Wpf.Ui.Common;
+using Wpf.Ui.Mvvm.Contracts;
+using Wpf.Ui.Mvvm.Services;
 
 namespace Unowhy_Tools_WPF.Views.Pages;
 
@@ -13,15 +20,51 @@ namespace Unowhy_Tools_WPF.Views.Pages;
 /// </summary>
 public partial class About : INavigableView<DashboardViewModel>
 {
+    private readonly ISnackbarService _snackbarService;
+
     public DashboardViewModel ViewModel
     {
         get;
     }
 
-    public About(DashboardViewModel viewModel)
+    public About(DashboardViewModel viewModel, ISnackbarService snackbarService)
     {
         ViewModel = viewModel;
-
         InitializeComponent();
+
+        _snackbarService = snackbarService;
+
+    }
+
+    public void Github_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        System.Diagnostics.Process.Start(new ProcessStartInfo
+        {
+            FileName = "https://github.com/STY1001/Unowhy-Tools",
+                        UseShellExecute = true
+        });
+    }
+
+    public void STY1001_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        System.Diagnostics.Process.Start(new ProcessStartInfo
+        {
+            FileName = "https://sty1001.cf",
+            UseShellExecute = true
+        });
+    }
+
+    public void Discord_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        System.Diagnostics.Process.Start(new ProcessStartInfo
+        {
+            FileName = "https://discord.com/invite/dw3ZJ9u7WS",
+            UseShellExecute = true
+        });
+    }
+
+    public void Update_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        _snackbarService.Show("Update checker", "Not done", SymbolRegular.ErrorCircle24, ControlAppearance.Danger);
     }
 }
