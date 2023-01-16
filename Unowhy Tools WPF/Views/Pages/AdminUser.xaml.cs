c9875e548096c84a4821519b67446909f030c2e3
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-01-16 21:20:10 +0100

    Not done

---
+++ Unowhy Tools WPF/Views/Pages/AdminUser.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+
+using Wpf.Ui.Common.Interfaces;
+using Unowhy_Tools_WPF.ViewModels;
+
+namespace Unowhy_Tools_WPF.Views.Pages;
+
+/// <summary>
+/// Interaction logic for Dashboard.xaml
+/// </summary>
+public partial class AdminUser : INavigableView<DashboardViewModel>
+{
+    public DashboardViewModel ViewModel
+    {
+        get;
+    }
+
+    public AdminUser(DashboardViewModel viewModel)
+    {
+        ViewModel = viewModel;
+
+        InitializeComponent();
+    }
+}
