a31834aa237fb3ca8c847ad942fa779481dc807d
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-01-17 16:07:39 +0100

    Not done

---
+++ Unowhy Tools WPF/Views/Pages/DrvConv.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+
+using Wpf.Ui.Common.Interfaces;
+using Unowhy_Tools_WPF.ViewModels;
+
+namespace Unowhy_Tools_WPF.Views.Pages;
+
+/// <summary>
+/// Interaction logic for Dashboard.xaml
+/// </summary>
+public partial class DrvConv : INavigableView<DashboardViewModel>
+{
+    public DashboardViewModel ViewModel
+    {
+        get;
+    }
+
+    public DrvConv(DashboardViewModel viewModel)
+    {
+        ViewModel = viewModel;
+
+        InitializeComponent();
+    }
+}
