19598dbdf5fbcbe7cc9d710bf995a01da062eacb
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-01-17 16:07:45 +0100

    Not done

---
+++ Unowhy Tools WPF/Views/Pages/DrvRest.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+
+
+using Wpf.Ui.Common.Interfaces;
+using Unowhy_Tools_WPF.ViewModels;
+
+namespace Unowhy_Tools_WPF.Views.Pages;
+
+/// <summary>
+/// Interaction logic for Dashboard.xaml
+/// </summary>
+public partial class DrvRest : INavigableView<DashboardViewModel>
+{
+    public DashboardViewModel ViewModel
+    {
+        get;
+    }
+
+    public DrvRest(DashboardViewModel viewModel)
+    {
+        ViewModel = viewModel;
+
+        InitializeComponent();
+    }
+}
