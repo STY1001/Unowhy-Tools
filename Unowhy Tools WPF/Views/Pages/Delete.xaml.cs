cd12840fb541d2cca1e8654e561a9d3261cd0a6a
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-01-22 00:07:56 +0100

    Lang system

--- Unowhy Tools WPF/Views/Pages/Delete.xaml.cs
+++ Unowhy Tools WPF/Views/Pages/Delete.xaml.cs
@@ -1,10 +1,7 @@
-﻿
-
-
-
-
-using Wpf.Ui.Common.Interfaces;
+﻿using Wpf.Ui.Common.Interfaces;
 using Unowhy_Tools_WPF.ViewModels;
+
+using static Unowhy_Tools_WPF.UTclass;
 
 namespace Unowhy_Tools_WPF.Views.Pages;
 
@@ -16,6 +13,37 @@ public partial class Delete : INavigableView<DashboardViewModel>
     public DashboardViewModel ViewModel
     {
         get;
+    }
+
+    public void applylang()
+    {
+        instprog_txt.Text = getlang("unprog");
+        instprog_desc.Text = "";
+        instprog_btn.Text = getlang("open");
+        entu_txt.Text = getlang("ent");
+        entu_desc.Text = getlang("descent");
+        entu_btn.Text = getlang("del");
+        hsq_txt.Text = getlang("delhis");
+        hsq_desc.Text = getlang("deschisdel");
+        hsq_btn.Text = getlang("uninstall");
+        aad_txt.Text = getlang("aadleave");
+        aad_desc.Text = getlang("descaadleave");
+        aad_btn.Text = getlang("disconnect");
+        hsmqf_txt.Text = getlang("delhism");
+        hsmqf_desc.Text = getlang("deschismdel");
+        hsmqf_btn.Text = getlang("delete");
+        tif_txt.Text = getlang("delti");
+        tif_desc.Text = getlang("desctidel");
+        tif_btn.Text = getlang("delete");
+        ridff_txt.Text = getlang("delridf");
+        ridff_desc.Text = getlang("descridf");
+        ridff_btn.Text = getlang("delete");
+        oemf_txt.Text = getlang("deloem");
+        oemf_desc.Text = getlang("descdeloem");
+        oemf_btn.Text = getlang("delete");
+        entf_txt.Text = getlang("delentf");
+        entf_desc.Text = getlang("descdelentf");
+        entf_btn.Text = getlang("delete");
     }
 
     public Delete(DashboardViewModel viewModel)
