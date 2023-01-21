e82b7b088c347bcd2954cd553aea5392533de52d
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-01-22 00:08:10 +0100

    Lang system

--- Unowhy Tools WPF/Views/Pages/DrvBack.xaml.cs
+++ Unowhy Tools WPF/Views/Pages/DrvBack.xaml.cs
@@ -1,10 +1,7 @@
-﻿
-
-
-
-
-using Wpf.Ui.Common.Interfaces;
+﻿using Wpf.Ui.Common.Interfaces;
 using Unowhy_Tools_WPF.ViewModels;
+
+using static Unowhy_Tools_WPF.UTclass;
 
 namespace Unowhy_Tools_WPF.Views.Pages;
 
@@ -16,6 +13,13 @@ public partial class DrvBack : INavigableView<DashboardViewModel>
     public DashboardViewModel ViewModel
     {
         get;
+    }
+
+    public void applylang()
+    {
+        labbkapp.Text = getlang("bkapp");
+        labpath.Text = getlang("bkpath");
+        bk_btn.Text = getlang("backup");
     }
 
     public DrvBack(DashboardViewModel viewModel)
