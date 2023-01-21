c76e327025b5b6b5824b61dde78734a56375a731
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-01-22 00:07:42 +0100

    Lang system

--- Unowhy Tools WPF/Views/Pages/AdminUser.xaml.cs
+++ Unowhy Tools WPF/Views/Pages/AdminUser.xaml.cs
@@ -1,10 +1,7 @@
-﻿
-
-
-
-
-using Wpf.Ui.Common.Interfaces;
+﻿using Wpf.Ui.Common.Interfaces;
 using Unowhy_Tools_WPF.ViewModels;
+
+using static Unowhy_Tools_WPF.UTclass;
 
 namespace Unowhy_Tools_WPF.Views.Pages;
 
@@ -16,6 +13,13 @@ public partial class AdminUser : INavigableView<DashboardViewModel>
     public DashboardViewModel ViewModel
     {
         get;
+    }
+
+    public void applylang()
+    {
+        snpw.Text = getlang("snpw");
+        en.Text = getlang("enablea");
+        dis.Text = getlang("disablea");
     }
 
     public AdminUser(DashboardViewModel viewModel)
