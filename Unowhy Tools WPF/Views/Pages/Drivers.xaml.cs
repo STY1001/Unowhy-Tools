aa256065d433dc35ae5a78617a8d02d01dd1e096
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-01-22 00:08:03 +0100

    Lang system

--- Unowhy Tools WPF/Views/Pages/Drivers.xaml.cs
+++ Unowhy Tools WPF/Views/Pages/Drivers.xaml.cs
@@ -1,10 +1,7 @@
-﻿
-
-
-
-
-using Wpf.Ui.Common.Interfaces;
+﻿using Wpf.Ui.Common.Interfaces;
 using Unowhy_Tools_WPF.ViewModels;
+
+using static Unowhy_Tools_WPF.UTclass;
 
 namespace Unowhy_Tools_WPF.Views.Pages;
 
@@ -16,6 +13,19 @@ public partial class Drivers : INavigableView<DashboardViewModel>
     public DashboardViewModel ViewModel
     {
         get;
+    }
+
+    public void applylang()
+    {
+        dlcloud_txt.Text = getlang("bkcloud");
+        dlcloud_desc.Text = getlang("descbkcloud");
+        dlcloud_btn.Text = getlang("dl");
+        bk_txt.Text = getlang("back");
+        bk_desc.Text = getlang("drvbr");
+        bk_btn.Text = getlang("backup");
+        rt_txt.Text = getlang("rest");
+        rt_desc.Text = getlang("drvbr");
+        rt_btn.Text = getlang("restore");
     }
 
     public Drivers(DashboardViewModel viewModel)
