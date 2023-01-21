d16096859daec464dcead737c3e4173d6931a325
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-01-22 00:07:49 +0100

    Lang system

--- Unowhy Tools WPF/Views/Pages/Customize.xaml.cs
+++ Unowhy Tools WPF/Views/Pages/Customize.xaml.cs
@@ -1,10 +1,7 @@
-﻿
-
-
-
-
-using Wpf.Ui.Common.Interfaces;
+﻿using Wpf.Ui.Common.Interfaces;
 using Unowhy_Tools_WPF.ViewModels;
+
+using static Unowhy_Tools_WPF.UTclass;
 
 namespace Unowhy_Tools_WPF.Views.Pages;
 
@@ -18,10 +15,27 @@ public partial class Customize : INavigableView<DashboardViewModel>
         get;
     }
 
+    public void applylang()
+    {
+        pcname_txt.Text = getlang("pcname");
+        pcname_desc.Text = getlang("descpcname");
+        pcname_btn.Text = getlang("change");
+        adminset_txt.Text = getlang("admin");
+        adminset_desc.Text = getlang("descadmin");
+        adminset_btn.Text = getlang("set");
+        adduser_txt.Text = getlang("adduser");
+        adduser_desc.Text = getlang("descadduser");
+        adduser_btn.Text = getlang("create");
+        auset_txt.Text = getlang("adminset");
+        auset_desc.Text = getlang("descadminset");
+        auset_btn.Text = getlang("open");
+    }
+
     public Customize(DashboardViewModel viewModel)
     {
         ViewModel = viewModel;
 
         InitializeComponent();
+        applylang();
     }
 }
