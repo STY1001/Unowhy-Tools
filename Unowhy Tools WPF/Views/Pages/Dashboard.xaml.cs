486e1c45f9d5ea050ccc080416497ee3181f9ec9
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-02-02 11:17:41 +0100

    Added features

--- Unowhy Tools WPF/Views/Pages/Dashboard.xaml.cs
+++ Unowhy Tools WPF/Views/Pages/Dashboard.xaml.cs
@@ -1,10 +1,8 @@
-﻿
-
-
-
-
-using Wpf.Ui.Common.Interfaces;
+﻿using Wpf.Ui.Common.Interfaces;
 using Unowhy_Tools_WPF.ViewModels;
+using System.Windows;
+
+using Unowhy_Tools;
 
 namespace Unowhy_Tools_WPF.Views.Pages;
 
@@ -13,6 +11,8 @@ namespace Unowhy_Tools_WPF.Views.Pages;
 /// </summary>
 public partial class Dashboard : INavigableView<DashboardViewModel>
 {
+    UT.Data UTdata = new UT.Data();
+
     public DashboardViewModel ViewModel
     {
         get;
@@ -23,5 +23,40 @@ public partial class Dashboard : INavigableView<DashboardViewModel>
         ViewModel = viewModel;
 
         InitializeComponent();
+
+        applylang();
+        pcname.Text = UT.getline(UTdata.HostName, 1);
+    }
+
+    public void applylang()
+    {
+        lababout.Text = UT.getlang("about");
+        labinfo.Text = UT.getlang("pcinfo");
+        labset.Text = UT.getlang("settings");
+    }
+
+    public void Taskmgr(object sender, RoutedEventArgs e)
+    {
+        System.Diagnostics.Process.Start("taskmgr.exe");
+    }
+    
+    public void CMD(object sender, RoutedEventArgs e)
+    {
+        System.Diagnostics.Process.Start("cmd.exe");
+    }
+    
+    public void Regedit(object sender, RoutedEventArgs e)
+    {
+        System.Diagnostics.Process.Start("regedit.exe");
+    }
+
+    public void Gpedit(object sender, RoutedEventArgs e)
+    {
+        System.Diagnostics.Process.Start("mmc.exe", "gpedit.msc");
+    }
+
+    public void Guide(object sender, RoutedEventArgs e)
+    {
+        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo { FileName = "https://github.com/STY1001/Unowhy-Tools/blob/master/GUIDE.md", UseShellExecute = true });
     }
 }
