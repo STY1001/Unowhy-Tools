ae897652272c5fd42bbc6cfbeacd53fb65c990e4
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-02-27 16:21:18 +0100

    Fix and Done

--- Unowhy Tools WPF/Views/Pages/Repair.xaml.cs
+++ Unowhy Tools WPF/Views/Pages/Repair.xaml.cs
@@ -4,6 +4,7 @@ using Unowhy_Tools_WPF.ViewModels;
 using Unowhy_Tools;
 using System.Threading.Tasks;
 using System;
+using System.Windows;
 
 namespace Unowhy_Tools_WPF.Views.Pages;
 
@@ -72,5 +73,106 @@ public partial class Repair : INavigableView<DashboardViewModel>
         ViewModel = viewModel;
 
         InitializeComponent();
+    }
+
+    public async void shell_Click(object sender, RoutedEventArgs e)
+    {
+        if (UT.DialogQShow(UT.GetLang("shell"), "explorer.png"))
+        {
+            await UT.waitstatus.open();
+            await UT.RunMin("reg", "add \"HKLM\\Software\\Microsoft\\Windows NT\\CurrentVersion\\WinLogon\" /v Shell /d explorer.exe /t REG_SZ /f");
+            await CheckBTN();
+            await UT.waitstatus.close();
+            if (!shell.IsEnabled)
+            {
+                UT.DialogIShow(UT.GetLang("done"), "yes.png");
+            }
+            else
+            {
+                UT.DialogIShow(UT.GetLang("failed"), "no.png");
+            }
+        }
+    }
+
+    public async void tis_Click(object sender, RoutedEventArgs e)
+    {
+        if (UT.DialogQShow(UT.GetLang("fixboot"), "script.png"))
+        {
+            await UT.waitstatus.open();
+            await UT.RunMin("rmdir", "/q /f \"c:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\silent_*.*\"");
+            await CheckBTN();
+            await UT.waitstatus.close();
+            if (!tis.IsEnabled)
+            {
+                UT.DialogIShow(UT.GetLang("done"), "yes.png");
+            }
+            else
+            {
+                UT.DialogIShow(UT.GetLang("failed"), "no.png");
+            }
+        }
+    }
+
+    public async void bim_Click(object sender, RoutedEventArgs e)
+    {
+        if (UT.DialogQShow(UT.GetLang("bootim"), "registry.png"))
+        {
+            await UT.waitstatus.open();
+            await UT.RunMin("reg", "delete \"HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\bootim.exe\" /f";
+            await CheckBTN();
+            await UT.waitstatus.close();
+            if (!bim.IsEnabled)
+            {
+                UT.DialogIShow(UT.GetLang("done"), "yes.png");
+            }
+            else
+            {
+                UT.DialogIShow(UT.GetLang("failed"), "no.png");
+            }
+        }
+    }
+
+    public async void whe_Click(object sender, RoutedEventArgs e)
+    {
+        if (UT.DialogQShow(UT.GetLang("enwhe"), "fp.png"))
+        {
+            await UT.waitstatus.open();
+            await UT.RunMin("reg", "add \"HKLM\\SOFTWARE\\Policies\\Microsoft\\PassportForWork\" /v \"EnablePinRecovery\" /t REG_DWORD /d \"1\" /f");
+            await UT.RunMin("reg", "add \"HKLM\\SOFTWARE\\Policies\\Microsoft\\PassportForWork\" /v \"RequireSecurityDevice\" /t REG_DWORD /d \"1\" /f");
+            await UT.RunMin("reg", "add \"HKLM\\SOFTWARE\\Policies\\Microsoft\\PassportForWork\" /v \"Enabled\" /t REG_DWORD /d \"1\" /f");
+            await UT.RunMin("reg", "add \"HKLM\\SOFTWARE\\Policies\\Microsoft\\PassportForWork\" /v \"DisablePostLogonProvisioning\" /t REG_DWORD /d \"0\" /f");
+            await UT.RunMin("reg", "add \"HKLM\\SOFTWARE\\Policies\\Microsoft\\PassportForWork\\DynamicLock\" /v \"DynamicLock\" /t REG_DWORD /d \"1\" /f");
+            await UT.RunMin("reg", "add \"HKLM\\SOFTWARE\\Policies\\Microsoft\\PassportForWork\\DynamicLock\" /v \"Plugins\" /t REG_SZ /d \"<rule schemaVersion=\\\"1.0\\\"> <signal type=\\\"bluetooth\\\" scenario=\\\"Dynamic Lock\\\" classOfDevice=\\\"512\\\" rssiMin=\\\"-10\\\" rssiMaxDelta=\\\"-10\\\"/> </rule> \" /f");
+            await UT.RunMin("reg", "add \"HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\WinBio\\Credential Provider\" /v \"Domain Accounts\" /t REG_DWORD /d \"1\" /f");
+            await CheckBTN();
+            await UT.waitstatus.close();
+            if (!whe.IsEnabled)
+            {
+                UT.DialogIShow(UT.GetLang("done"), "yes.png");
+            }
+            else
+            {
+                UT.DialogIShow(UT.GetLang("failed"), "no.png");
+            }
+        }
+    }
+
+    public async void iaf_Click(object sender, RoutedEventArgs e)
+    {
+        if (UT.DialogQShow(UT.GetLang("bcdfail"), "boot.png"))
+        {
+            await UT.waitstatus.open();
+            await UT.RunMin("bcdedit", "/deletevalue bootstatuspolicy");
+            await CheckBTN();
+            await UT.waitstatus.close();
+            if (!iaf.IsEnabled)
+            {
+                UT.DialogIShow(UT.GetLang("done"), "yes.png");
+            }
+            else
+            {
+                UT.DialogIShow(UT.GetLang("failed"), "no.png");
+            }
+        }
     }
 }
