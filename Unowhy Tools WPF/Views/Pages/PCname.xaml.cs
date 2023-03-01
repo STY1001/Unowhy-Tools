d055013b57fef98dc5927db6c1eb353ca1cdf22c
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-03-01 20:46:36 +0100

    Done

--- Unowhy Tools WPF/Views/Pages/PCname.xaml.cs
+++ Unowhy Tools WPF/Views/Pages/PCname.xaml.cs
@@ -2,6 +2,9 @@
 using Unowhy_Tools_WPF.ViewModels;
 
 using Unowhy_Tools;
+using System.Windows;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace Unowhy_Tools_WPF.Views.Pages;
 
@@ -37,5 +40,26 @@ public partial class PCname : INavigableView<DashboardViewModel>
 
         applylang();
         CheckBTN();
+    }
+
+    public async void Change_Click(object sender, RoutedEventArgs e)
+    {
+        Regex r = new Regex(@"[~`!@#$%^&*()+=|\\{}':;.,<>/?[\]""]");
+
+        if (r.IsMatch(newbox.Text.ToString().Trim()) || newbox.Text.Length < 2 || newbox.Text.Length > 15 || newbox.Text.Contains(" "))
+        {
+            UT.DialogIShow(UT.GetLang("avert"), "no.png")
+        }
+        else
+        {
+            if (UT.DialogQShow(UT.GetLang("pcname"), "customize.png"))
+            {
+                string name = newbox.Text;
+                string arg = ($"-Command \"& {{Rename-Computer -NewName \"{name}\" -Force}}\"");
+
+                await UT.RunMin("powershell", arg);
+                old.Text = await UT.RunReturn("hostname", "");
+            }
+        }
     }
 }
