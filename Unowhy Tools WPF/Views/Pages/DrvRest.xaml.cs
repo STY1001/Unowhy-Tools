8ffd1996ea92c5c2a165438bb0e215935ab7a5be
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-03-01 19:03:25 +0100

    Done

--- Unowhy Tools WPF/Views/Pages/DrvRest.xaml.cs
+++ Unowhy Tools WPF/Views/Pages/DrvRest.xaml.cs
@@ -2,6 +2,12 @@
 using Unowhy_Tools_WPF.ViewModels;
 
 using Unowhy_Tools;
+using System.Windows;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.Diagnostics;
 
 namespace Unowhy_Tools_WPF.Views.Pages;
 
@@ -30,5 +36,72 @@ public partial class DrvRest : INavigableView<DashboardViewModel>
         InitializeComponent();
 
         applylang();
+    }
+
+    public void Browse_Click(object sender, RoutedEventArgs e)
+    {
+        using (var fb = new OpenFileDialog())
+        {
+            fb.DefaultExt = "zip";
+            fb.Filter = "zip (*.zip)|*.zip";
+            fb.FilterIndex = 1;
+            fb.Title = "Unowhy Tools";
+            DialogResult result = fb.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                rtpath.Text = fb.FileName;
+            }
+        }
+    }
+
+    public async void Restore_Click(object sender, RoutedEventArgs e)
+    {
+        if (rtpath.Text == "")
+        {
+
+        }
+        else
+        {
+            DriveInfo di = new DriveInfo("C");
+            FileInfo fi = new FileInfo(rtpath.Text);
+            if (di.AvailableFreeSpace > fi.Length * 2.5)
+            {
+                await UT.waitstatus.open();
+                await Task.Delay(1000);
+                string rttemps = Path.GetTempPath() + "\\Unowhy Tools\\Temps\\Drivers";
+                await Task.Run(() =>
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        ZipFile.ExtractToDirectory(rtpath.Text, rttemps);
+                    });
+                });
+                if (File.Exists(rttemps + "\\UT-Restore.exe"))
+                {
+                    await Task.Run(() =>
+                    {
+                        Process p = new Process();
+                        p.StartInfo.FileName = rttemps + "\\UT-Restore.exe";
+                        p.StartInfo.WorkingDirectory = rttemps;
+                        p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        p.StartInfo.CreateNoWindow = true;
+                        p.Start();
+                        p.WaitForExit();
+                    });
+
+                }
+                else
+                {
+                    UT.DialogIShow(UT.GetLang("conv.nout"), "no.png");
+                }
+                Directory.Delete(rttemps, true);
+                Directory.CreateDirectory(rttemps);
+                UT.waitstatus.close();
+            }
+            else
+            {
+                UT.DialogIShow(UT.GetLang("space6gpc"), "no.png");
+            }
+        }
     }
 }
