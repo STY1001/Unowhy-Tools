3f4665e1e05a467d4914c7fc37b2a7abeacc343b
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-03-01 11:18:18 +0100

    Done

--- Unowhy Tools WPF/Views/Pages/AddUser.xaml.cs
+++ Unowhy Tools WPF/Views/Pages/AddUser.xaml.cs
@@ -2,6 +2,12 @@
 using Unowhy_Tools_WPF.ViewModels;
 
 using Unowhy_Tools;
+using System.Windows;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Windows.Forms;
 
 namespace Unowhy_Tools_WPF.Views.Pages;
 
@@ -31,5 +37,87 @@ public partial class AddUser : INavigableView<DashboardViewModel>
         InitializeComponent();
 
         applylang();
+    }
+
+    public void pass_Changed(object sender, RoutedEventArgs e)
+    {
+        if (passbox1.Text == "")
+        {
+            pimg1.Source = UT.GetImgSource("passno.png");
+        }
+        else
+        {
+            pimg1.Source = UT.GetImgSource("passyes.png");
+        }
+
+        if (passbox1.Text == passbox2.Text)
+        {
+            pimg2.Source = UT.GetImgSource("pwcyes.png");
+        }
+        else
+        {
+            pimg2.Source = UT.GetImgSource("pwcno.png");
+        }
+    }
+
+    public void name_Changed(object sender, RoutedEventArgs e)
+    {
+        Regex r = new Regex(@"[~`!@#$%^&*()+=|\\{}':;.,<>/?[\]""]");
+
+        if (r.IsMatch(namebox.Text.ToString().Trim()) || namebox.Text == "" || namebox.Text.Contains(" "))
+        {
+            uimg1.Source = UT.GetImgSource("deluser.png");
+        }
+        else
+        {
+            uimg1.Source = UT.GetImgSource("user.png");
+        }
+    }
+    
+    public async void Create_Changed(object sender, RoutedEventArgs e)
+    {
+        Regex r = new Regex(@"[~`!@#$%^&*()+=|\\{}':;.,<>/?[\]""]");
+
+        if (r.IsMatch(namebox.Text.ToString().Trim()) || namebox.Text == "" || namebox.Text.Contains(" "))
+        {
+            UT.DialogIShow(UT.GetLang("an"), "no.png");
+        }
+        else
+        {
+            if (passbox1.Text == passbox2.Text)
+            {
+                if (UT.DialogQShow(UT.GetLang("adduser"), "adduser.png")
+                {
+                    await UT.waitstatus.open();
+
+                    if (passbox1.Text == "")
+                    {
+                        await UT.RunMin("net", $"user \"{namebox.Text}\" /add");
+
+                        if (laba.IsChecked == true)
+                        {
+                            await UT.RunMin("net", $"localgroup Administrateurs \"{namebox.Text}\" /add");
+                        }
+                    }
+                    else
+                    {
+                        await UT.RunMin("net", $"user \"{namebox.Text}\" \"{passbox1.Text}\" /add");
+
+                        if (laba.IsChecked == true)
+                        {
+                            await UT.RunMin("net", $"localgroup Administrateurs \"{namebox.Text}\" /add");
+                        }
+                    }
+
+                    await UT.waitstatus.close();
+
+                    UT.DialogIShow(UT.GetLang("adduser.id") + $": \"{namebox.Text}\"", "user.png");
+                }
+            }
+            else
+            {
+                UT.DialogIShow(UT.GetLang("pwmis"), "no.png");
+            }
+        }
     }
 }
