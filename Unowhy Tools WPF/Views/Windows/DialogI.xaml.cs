e89a05bd45d5606ff5b59242671ee489afbbc455
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-03-05 15:34:11 +0100

    New animation

--- Unowhy Tools WPF/Views/Windows/DialogI.xaml.cs
+++ Unowhy Tools WPF/Views/Windows/DialogI.xaml.cs
@@ -10,6 +10,9 @@ using System.Threading;
 using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System;
+using System.Windows.Media;
 
 namespace Unowhy_Tools_WPF.Views.Windows
 {
@@ -22,6 +25,8 @@ namespace Unowhy_Tools_WPF.Views.Windows
         {
             InitializeComponent();
             Visibility = Visibility.Hidden;
+            //Border1.Opacity = 0;
+            //Border1.LayoutTransform = new ScaleTransform(0, 0);
         }
 
         private bool _hideRequest = false;
@@ -33,6 +38,45 @@ namespace Unowhy_Tools_WPF.Views.Windows
             icon.Source = image;
             text.Text = message;
             Visibility = Visibility.Visible;
+            var fadeInAnimation = new DoubleAnimation
+            {
+                From = 0,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(0.25),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseIn }
+            };
+
+            var zoomAnimation1 = new DoubleAnimation
+            {
+                From = 0.50,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(0.15),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+            
+            var zoomAnimation2 = new DoubleAnimation
+            {
+                From = 0.50,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(0.15),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            Storyboard.SetTarget(fadeInAnimation, Border1);
+            Storyboard.SetTargetProperty(fadeInAnimation, new PropertyPath(OpacityProperty));
+
+            Storyboard.SetTarget(zoomAnimation1, Border1);
+            Storyboard.SetTarget(zoomAnimation2, Border1);
+            Storyboard.SetTargetProperty(zoomAnimation1, new PropertyPath("(UIElement.RenderTransform).(ScaleTransform.ScaleX)"));
+            Storyboard.SetTargetProperty(zoomAnimation2, new PropertyPath("(UIElement.RenderTransform).(ScaleTransform.ScaleY)"));
+
+            var storyboard = new Storyboard();
+            storyboard.Children.Add(fadeInAnimation);
+            storyboard.Children.Add(zoomAnimation1);
+            storyboard.Children.Add(zoomAnimation2);
+
+            storyboard.Begin();
+
             _hideRequest = false;
             while (!_hideRequest)
             {
@@ -53,14 +97,58 @@ namespace Unowhy_Tools_WPF.Views.Windows
 
         private void HideDialog()
         {
-            _hideRequest = true;
+            var fadeInAnimation2 = new DoubleAnimation
+            {
+                From = 1,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.1),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            var zoomAnimation12 = new DoubleAnimation
+            {
+                From = 1,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.25),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            var zoomAnimation22 = new DoubleAnimation
+            {
+                From = 1,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.25),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            Storyboard.SetTarget(fadeInAnimation2, Border1);
+            Storyboard.SetTargetProperty(fadeInAnimation2, new PropertyPath(OpacityProperty));
+
+            Storyboard.SetTarget(zoomAnimation12, Border1);
+            Storyboard.SetTarget(zoomAnimation22, Border1);
+            Storyboard.SetTargetProperty(zoomAnimation12, new PropertyPath("(UIElement.RenderTransform).(ScaleTransform.ScaleX)"));
+            Storyboard.SetTargetProperty(zoomAnimation22, new PropertyPath("(UIElement.RenderTransform).(ScaleTransform.ScaleY)"));
+
+            var storyboard = new Storyboard();
+            storyboard.Children.Add(fadeInAnimation2);
+            storyboard.Children.Add(zoomAnimation12);
+            storyboard.Children.Add(zoomAnimation22);
+
+            storyboard.Completed += RealHideDialog;
+
+            storyboard.Begin();
+        }
+
+        private void RealHideDialog(object sender, EventArgs e)
+        {
             Visibility = Visibility.Hidden;
+            _hideRequest = true;
         }
 
         private void OkButton_Click(object sender, RoutedEventArgs e)
         {
-            _result = true;
             HideDialog();
+            _result = true;
         }
     }
 }
--- Unowhy Tools WPF/Views/Windows/DialogQ.xaml.cs
+++ Unowhy Tools WPF/Views/Windows/DialogQ.xaml.cs
@@ -10,6 +10,8 @@ using System.Threading;
 using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System;
 
 namespace Unowhy_Tools_WPF.Views.Windows
 {
@@ -35,6 +37,45 @@ namespace Unowhy_Tools_WPF.Views.Windows
             text.Text = message + " ?";
             Visibility = Visibility.Visible;
 
+            var fadeInAnimation = new DoubleAnimation
+            {
+                From = 0,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(0.25),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseIn }
+            };
+
+            var zoomAnimation1 = new DoubleAnimation
+            {
+                From = 0.50,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(0.15),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            var zoomAnimation2 = new DoubleAnimation
+            {
+                From = 0.50,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(0.15),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            Storyboard.SetTarget(fadeInAnimation, Border1);
+            Storyboard.SetTargetProperty(fadeInAnimation, new PropertyPath(OpacityProperty));
+
+            Storyboard.SetTarget(zoomAnimation1, Border1);
+            Storyboard.SetTarget(zoomAnimation2, Border1);
+            Storyboard.SetTargetProperty(zoomAnimation1, new PropertyPath("(UIElement.RenderTransform).(ScaleTransform.ScaleX)"));
+            Storyboard.SetTargetProperty(zoomAnimation2, new PropertyPath("(UIElement.RenderTransform).(ScaleTransform.ScaleY)"));
+
+            var storyboard = new Storyboard();
+            storyboard.Children.Add(fadeInAnimation);
+            storyboard.Children.Add(zoomAnimation1);
+            storyboard.Children.Add(zoomAnimation2);
+
+            storyboard.Begin();
+
             _hideRequest = false;
             while (!_hideRequest)
             {
@@ -54,6 +95,50 @@ namespace Unowhy_Tools_WPF.Views.Windows
         }
 
         private void HideDialog()
+        {
+            var fadeInAnimation2 = new DoubleAnimation
+            {
+                From = 1,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.1),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            var zoomAnimation12 = new DoubleAnimation
+            {
+                From = 1,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.25),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            var zoomAnimation22 = new DoubleAnimation
+            {
+                From = 1,
+                To = 0,
+                Duration = TimeSpan.FromSeconds(0.25),
+                EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+            };
+
+            Storyboard.SetTarget(fadeInAnimation2, Border1);
+            Storyboard.SetTargetProperty(fadeInAnimation2, new PropertyPath(OpacityProperty));
+
+            Storyboard.SetTarget(zoomAnimation12, Border1);
+            Storyboard.SetTarget(zoomAnimation22, Border1);
+            Storyboard.SetTargetProperty(zoomAnimation12, new PropertyPath("(UIElement.RenderTransform).(ScaleTransform.ScaleX)"));
+            Storyboard.SetTargetProperty(zoomAnimation22, new PropertyPath("(UIElement.RenderTransform).(ScaleTransform.ScaleY)"));
+
+            var storyboard = new Storyboard();
+            storyboard.Children.Add(fadeInAnimation2);
+            storyboard.Children.Add(zoomAnimation12);
+            storyboard.Children.Add(zoomAnimation22);
+
+            storyboard.Completed += RealHideDialog;
+
+            storyboard.Begin();
+        }
+
+        private void RealHideDialog(object sender, EventArgs e)
         {
             _hideRequest = true;
             Visibility = Visibility.Hidden;
