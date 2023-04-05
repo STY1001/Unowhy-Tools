476f78f464eae6a4b09046626887e1f3bef10fee
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-04-05 20:17:08 +0200

    23.00

--- Unowhy Tools Installer/Main.Designer.cs
+++ Unowhy Tools Installer/Main.Designer.cs
@@ -191,7 +191,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 39;
-            this.label2.Text = "Ver 22.00";
+            this.label2.Text = "Ver 23.00";
             // 
             // pictureBox6
             // 
