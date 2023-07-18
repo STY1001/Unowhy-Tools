f2fc0c1e867726601d4af967e435d64f41e28544
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-07-18 22:07:34 +0200

    Starting 24.09

--- Unowhy Tools WPF/UT.cs
+++ Unowhy Tools WPF/UT.cs
@@ -42,9 +42,9 @@ namespace Unowhy_Tools
         private static extern bool InternetGetConnectedState(out int state, int value);
         #endregion
 
-        public static int verfull = 2408;
+        public static int verfull = 2409;
         public static string verbuild = "1250180723";
-        public static bool verisdeb = false;
+        public static bool verisdeb = true;
 
         public class version
         {
