335bae892978d9328967020c02ef300d07785c53
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-07-22 00:37:27 +0200

    Fix

--- Unowhy Tools WPF/UT.cs
+++ Unowhy Tools WPF/UT.cs
@@ -96,7 +96,7 @@ namespace Unowhy_Tools
 
             public static async Task AnimParent(string animtype)
             {
-                switch(animtype)
+                switch (animtype)
                 {
                     case "zoomin":
                         await BorderZoomIn(_parentborder);
@@ -564,7 +564,7 @@ namespace Unowhy_Tools
                         if (sc.Status == ServiceControllerStatus.Running)
                         {
                             await MainWindow.USSwB("Preparing UTS... (Running)");
-                            
+
                         }
                         else
                         {
@@ -599,32 +599,29 @@ namespace Unowhy_Tools
                     string newver = await web.GetStringAsync("https://bit.ly/UTStext");
                     newver = newver.Replace("\n", "").Replace("\r", "").Replace(" ", "");
 
-                    if (!verisdeb)
-                    {
-                        string ver = await UT.UTS.UTSmsg("UTS", "GetVer");
+                    string ver = await UT.UTS.UTSmsg("UTS", "GetVer");
 
-                        if (!(newver == ver))
-                        {
-                            await MainWindow.USSwB("Preparing UTS... (" + ver + " => " + newver + ")");
-                            await Task.Delay(1000);
-                            await MainWindow.USSwB("Preparing UTS... (Shutting down)");
-                            await UT.serv.stop("UTS");
-                            await MainWindow.USSwB("Preparing UTS... (Downloading)");
-                            string instdir = Directory.GetCurrentDirectory() + "\\Unowhy Tools Service";
-                            Directory.Delete(instdir, true);
-                            await Task.Delay(100);
-                            Directory.CreateDirectory(instdir);
-                            web = new HttpClient();
-                            var filebyte = await web.GetByteArrayAsync("https://bit.ly/UTSzip");
-                            string utemp = Path.GetTempPath() + "Unowhy Tools\\Temps";
-                            await File.WriteAllBytesAsync(utemp + "\\service.zip", filebyte);
-                            await MainWindow.USSwB("Preparing UTS... (Extracting)");
-                            await Task.Delay(100);
-                            ZipFile.ExtractToDirectory(utemp + "\\service.zip", instdir);
-                            await Task.Delay(100);
-                            await MainWindow.USSwB("Preparing UTS... (Starting up)");
-                            await UT.serv.start("UTS");
-                        }
+                    if (!(newver == ver) || verisdeb)
+                    {
+                        await MainWindow.USSwB("Preparing UTS... (" + ver + " => " + newver + ")");
+                        await Task.Delay(1000);
+                        await MainWindow.USSwB("Preparing UTS... (Shutting down)");
+                        await UT.serv.stop("UTS");
+                        await MainWindow.USSwB("Preparing UTS... (Downloading)");
+                        string instdir = Directory.GetCurrentDirectory() + "\\Unowhy Tools Service";
+                        Directory.Delete(instdir, true);
+                        await Task.Delay(100);
+                        Directory.CreateDirectory(instdir);
+                        web = new HttpClient();
+                        var filebyte = await web.GetByteArrayAsync("https://bit.ly/UTSzip");
+                        string utemp = Path.GetTempPath() + "Unowhy Tools\\Temps";
+                        await File.WriteAllBytesAsync(utemp + "\\service.zip", filebyte);
+                        await MainWindow.USSwB("Preparing UTS... (Extracting)");
+                        await Task.Delay(100);
+                        ZipFile.ExtractToDirectory(utemp + "\\service.zip", instdir);
+                        await Task.Delay(100);
+                        await MainWindow.USSwB("Preparing UTS... (Starting up)");
+                        await UT.serv.start("UTS");
                     }
                 }
             }
@@ -767,7 +764,7 @@ namespace Unowhy_Tools
             bool tray;
             TaskScheduler.TaskService taskService = new TaskScheduler.TaskService();
             TaskScheduler.Task uttask = taskService.GetTask("Unowhy Tools Tray Launch");
-            if(uttask != null)
+            if (uttask != null)
             {
                 if (uttask.Definition.Settings.Enabled)
                 {
@@ -804,7 +801,7 @@ namespace Unowhy_Tools
                         Write2Log("Sending Stats to \"" + url + "\" with \"" + jsonData + "\"");
                         StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
                         HttpResponseMessage response = await client.PostAsync(url, content);
-                        if(response.StatusCode == HttpStatusCode.OK) 
+                        if (response.StatusCode == HttpStatusCode.OK)
                         {
                             Write2Log("Sent! Response:" + response.Content + "(" + response.StatusCode + ")");
                         }
@@ -817,7 +814,7 @@ namespace Unowhy_Tools
                     {
                         Write2Log("Failed: " + e);
                     }
-                    
+
                 }
             }
         }
