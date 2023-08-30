0324101e216d8bb88f65d645416983efdd9b03a9
STY1001 <57889852+STY1001@users.noreply.github.com>
2023-08-30 15:14:07 +0300

    New build

--- Unowhy Tools WPF/UT.cs
+++ Unowhy Tools WPF/UT.cs
@@ -150,7 +150,7 @@ namespace Unowhy_Tools
         #endregion
 
         public static int verfull = 2500;
-        public static string verbuild = "1410300823";
+        public static string verbuild = "1515300823";
         public static bool verisdeb = true;
 
         public class version
