ed714e6d29f96b6f2c28ea9f402eaf5288aa9611
STY1001 <57889852+STY1001@users.noreply.github.com>
2024-01-13 20:51:18 +0100

    Fix

--- Unowhy Tools WPF/UT.cs
+++ Unowhy Tools WPF/UT.cs
@@ -158,7 +158,7 @@ namespace Unowhy_Tools
         public static string utpath = "C:\\Unowhy Tools";
 
         public static int verfull = 2800;
-        public static string verbuild = "2040130124";
+        public static string verbuild = "2050130124";
         public static bool verisdeb = true;
 
         public class version
