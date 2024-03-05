83060e69c03e821c7294dc55673cc7edd21ee635
STY1001 <57889852+STY1001@users.noreply.github.com>
2024-03-05 16:54:25 +0100

    fix

--- Unowhy Tools WPF/Views/MainWindow.xaml.cs
+++ Unowhy Tools WPF/Views/MainWindow.xaml.cs
@@ -441,7 +441,7 @@ public partial class MainWindow : INavigationWindow
         await mainWindow.updatesplashstatus(status, true);
     }
 
-    public int totalstep = 57;
+    public int totalstep = 63;
     public int actualstep = 0;
 
     public async Task updatesplashstatus(string status, bool bar)
