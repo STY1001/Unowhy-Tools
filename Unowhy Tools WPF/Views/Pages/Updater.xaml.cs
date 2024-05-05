50904289fc32168bead5b043b160a8abc99ff289
STY1001 <57889852+STY1001@users.noreply.github.com>
2024-05-05 17:26:55 +0200

    Upgraded Unowhy Tools Updater

--- Unowhy Tools WPF/Views/Pages/Updater.xaml.cs
+++ Unowhy Tools WPF/Views/Pages/Updater.xaml.cs
@@ -18,6 +18,7 @@ using static Unowhy_Tools.UT;
 using System.Net;
 using System.Reflection.Metadata;
 using System.Threading;
+using Microsoft.Win32.TaskScheduler;
 
 namespace Unowhy_Tools_WPF.Views.Pages;
 
@@ -36,9 +37,9 @@ public partial class Updater : INavigableView<DashboardViewModel>
     public async void GoBack(object sender, RoutedEventArgs e)
     {
         //UT.anim.BackBtnAnim(BackBTN);
-        await Task.Delay(150);
+        await System.Threading.Tasks.Task.Delay(150);
         //UT.anim.TransitionBack(RootGrid);
-        await Task.Delay(200);
+        await System.Threading.Tasks.Task.Delay(200);
         UT.NavigateTo(typeof(About));
     }
 
@@ -101,7 +102,7 @@ public partial class Updater : INavigableView<DashboardViewModel>
 
         trans.BeginAnimation(TranslateTransform.YProperty, anim);
 
-        await Task.Delay(600);
+        await System.Threading.Tasks.Task.Delay(600);
 
         gitbtn.Click -= GithubButton_Click;
         gitbtn.Click += GithubButton2_Click;
@@ -132,7 +133,7 @@ public partial class Updater : INavigableView<DashboardViewModel>
 
         trans.BeginAnimation(TranslateTransform.XProperty, anim);
 
-        await Task.Delay(600);
+        await System.Threading.Tasks.Task.Delay(600);
 
         gitbtn.Click += GithubButton_Click;
         gitbtn.Click -= GithubButton2_Click;
@@ -170,9 +171,9 @@ public partial class Updater : INavigableView<DashboardViewModel>
             trans.BeginAnimation(TranslateTransform.YProperty, anim);
         }
 
-        await Task.Delay(600);
+        await System.Threading.Tasks.Task.Delay(600);
         
-        if (await UT.version.newver() || UT.version.isdeb())
+        if (await UT.version.newver())
         {
             labimg.Source = UT.GetImgSource("yes.png");
             var web = new HttpClient();
@@ -180,6 +181,20 @@ public partial class Updater : INavigableView<DashboardViewModel>
             newver = newver.Insert(2, ".");
             newver = newver.Replace("\n", "");
             string newverfull = await UT.GetLang("newver") + " (" + UT.version.getverfull().ToString().Insert(2, ".") + " -> " + newver + ")";
+            labtext.Text = newverfull;
+            UpdateBTN.Click -= CheckButton_Click;
+            UpdateBTN.Click += InstallButton_Click;
+            updatebtnimg.Source = UT.GetImgSource("download.png");
+            updatebtntext.Text = await UT.GetLang("unow");
+        }
+        else if (UT.version.isdeb())
+        {
+            labimg.Source = UT.GetImgSource("yes.png");
+            var web = new HttpClient();
+            string newver = await UT.OnlineDatas.GetUpdates("utnewver");
+            newver = newver.Insert(2, ".");
+            newver = newver.Replace("\n", "");
+            string newverfull = "Update to latest release (" + UT.version.getverfull().ToString().Insert(2, ".") + " -> " + newver + ")";
             labtext.Text = newverfull;
             UpdateBTN.Click -= CheckButton_Click;
             UpdateBTN.Click += InstallButton_Click;
@@ -202,35 +217,67 @@ public partial class Updater : INavigableView<DashboardViewModel>
         UpdateBTN.IsEnabled = false;
         labimg.Source = UT.GetImgSource("clouddl.png");
         labtext.Text = await UT.GetLang("update.dl");
-        await Task.Delay(1000);
+        await System.Threading.Tasks.Task.Delay(1000);
         string uddl = await UT.GetLang("update.dl");
         var progress = new System.Progress<double>();
         var cancellationToken = new CancellationTokenSource();
         progress.ProgressChanged += (sender, value) =>
         {
-            labtext.Text = uddl + " 1/2 (" + value.ToString("##0") + "%)";
+            labtext.Text = uddl + " (" + value.ToString("##0") + "%)";
         };
         await UT.DlFilewithProgress(await UT.OnlineDatas.GetUrls("utupdatezip"), utemp + "\\update.zip", progress, cancellationToken.Token);
         progress = new System.Progress<double>();
         cancellationToken = new CancellationTokenSource();
         progress.ProgressChanged += (sender, value) =>
         {
-            labtext.Text = uddl + " 2/2 (" + value.ToString("##0") + "%)";
+            labtext.Text = uddl + " (" + value.ToString("##0") + "%)";
         };
-        await UT.DlFilewithProgress(await UT.OnlineDatas.GetUrls("utuninstaller"), utemp + "\\Update\\uninstall.exe", progress, cancellationToken.Token);
+        await UT.DlFilewithProgress(await UT.OnlineDatas.GetUrls("utszip"), utemp + "\\service.zip", progress, cancellationToken.Token);
         labtext.Text = await UT.GetLang("update.ext");
         labimg.Source = UT.GetImgSource("zip.png");
-        await Task.Delay(1000);
+        await System.Threading.Tasks.Task.Delay(500);
         ZipFile.ExtractToDirectory(utemp + "\\update.zip", utemp + "\\Update", true);
-
+        labtext.Text = await UT.GetLang("update.sext");
+        await System.Threading.Tasks.Task.Delay(500);
+        Directory.CreateDirectory(utemp + "\\Update\\Unowhy Tools Service");
+        ZipFile.ExtractToDirectory(utemp + "\\service.zip", utemp + "\\Update\\Unowhy Tools Service", true);
         labtext.Text = await UT.GetLang("update.updating");
         labimg.Source = UT.GetImgSource("update.png");
-        await Task.Delay(1000);
+        await System.Threading.Tasks.Task.Delay(1000);
         string pre = utemp + "\\update";
         string post = Directory.GetCurrentDirectory();
 
-        Process.Start("cmd.exe", $"/c echo Updating Unowhy Tools... & taskkill /f /im \"Unowhy Tools.exe\" & net stop UTS & timeout -t 3 & del /s /q \"{post}\\*\" & xcopy \"{pre}\" \"{post}\" /e /h /c /i /y & echo Done ! & powershell -windows hidden -command \"\" & \"Unowhy Tools.exe\" -user {UTdata.UserID}");
+        bool trayenabled = false;
+        TaskService ts = new TaskService();
+        Microsoft.Win32.TaskScheduler.Task uttltask = ts.GetTask("Unowhy Tools Tray Launch");
+        if (uttltask != null)
+        {
+            if (uttltask.Definition.Settings.Enabled)
+            {
+                trayenabled = true;
+            }
+            else
+            {
+                trayenabled = false;
+            }
+        }
+        else
+        {
+            trayenabled = false;
+        }
 
+        string args = $"-user {UTdata.UserID} ";
+        if (UTdata.RunConsole)
+        {
+            args = args + "-console ";
+        }
+
+        if (UTdata.RunUpdater && trayenabled)
+        {
+            args = args + "-tray ";
+        }
+
+        Process.Start("cmd.exe", $"/c echo Unowhy Tools by STY1001 & echo Please wait, Unowhy Tools is updating... & echo Killing Unowhy Tools... & taskkill /f /im \"Unowhy Tools.exe\" & echo Stopping Unowhy Tools Service... & net stop UTS & timeout -t 3 & echo Copying files... & del /s /q \"{post}\\*\" & xcopy \"{pre}\" \"{post}\" /e /h /c /i /y & cls & echo Update done... & echo Starting Unowhy Tools Service... & net start UTS & echo Restarting Unowhy Tools... & powershell -windows hidden -command \"\" & \"Unowhy Tools.exe\" {args}");
     }
 }
 
