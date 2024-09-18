f003e3c4b4204f0a63e96afc3e694b41d36ce7ef
STY1001 <57889852+STY1001@users.noreply.github.com>
2024-09-18 22:29:18 +0200

    I think this build is a RC

--- Unowhy Tools WPF/UT.cs
+++ Unowhy Tools WPF/UT.cs
@@ -165,7 +165,7 @@ namespace Unowhy_Tools
         public static string utpath = "C:\\Unowhy Tools";
 
         public static int verfull = 3000;
-        public static string verbuild = "2111180924";
+        public static string verbuild = "2228180924";
         public static bool verisdeb = true;
 
         public class version
