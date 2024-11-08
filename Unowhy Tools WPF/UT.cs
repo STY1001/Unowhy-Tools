6d4a2cd5ad2d1e5ca48d5eb19fe4e462085d60a8
STY1001 <57889852+STY1001@users.noreply.github.com>
2024-11-08 22:46:37 +0100

    31.00 releasing... yeah!

--- Unowhy Tools WPF/UT.cs
+++ Unowhy Tools WPF/UT.cs
@@ -159,7 +159,7 @@ namespace Unowhy_Tools
 
         public static int verfull = 3100;
         public static string verbuild = "2233081124";
-        public static bool verisdeb = true;
+        public static bool verisdeb = false;
 
         public static Dictionary<string, string> skumodel = new Dictionary<string, string>
         {
