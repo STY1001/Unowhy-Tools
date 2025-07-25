c3a6c0bb43c0ea45ffc6b2b9d0f1dd795d42da7e
STY1001 <57889852+STY1001@users.noreply.github.com>
2025-07-25 23:34:25 +0200

    New build

--- Unowhy Tools WPF/UT.cs
+++ Unowhy Tools WPF/UT.cs
@@ -157,9 +157,9 @@ namespace Unowhy_Tools
         public static string online_datas = "https://raw.githubusercontent.com/STY1001/Unowhy-Tools/master/Update/datas.json";
         public static string utpath = "C:\\Unowhy Tools";
 
-        public static int verfull = 3104;
-        public static string verbuild = "2340240625";
-        public static bool verisdeb = false;
+        public static int verfull = 3105;
+        public static string verbuild = "2334250725";
+        public static bool verisdeb = true;
 
         public static Dictionary<string, string> skumodel = new Dictionary<string, string>
         {
